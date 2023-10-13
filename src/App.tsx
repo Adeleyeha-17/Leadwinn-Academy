4cf7d05eb12275d29a6446b371383c23e70c8a2c
unknown <Adeleyeha.17@protonmail.com>
2023-10-13 15:57:08 +0100

    Tailwind and React icons installed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h2 className="flex items-center justify-center h-screen text-blue-500 font-bold text-3xl">LeadWinn Official Website</h2>
+    </>
+  )
+}
+
+export default App
