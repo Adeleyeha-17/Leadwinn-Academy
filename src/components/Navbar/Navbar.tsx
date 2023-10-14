df806328d707f62e7f5e0d8b584e9edf336e5f78
unknown <Adeleyeha.17@protonmail.com>
2023-10-14 17:21:36 +0100

    updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,19 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export const Navbar: React.FC = () => {
-   const [nav, setNav] = useState(false)
+   const [nav, setNav] = useState(false);
 
    const toggleFunc = () => {
-      setNav((prevState => !prevState));
-   }
+      setNav((prevState) => !prevState);
+   };
+
    return (
-      <>
-         <nav className="flex justify-between items-center h-16 px-5 pt-10 md:max-xl:px-7 md:max-w-6xl lg:max-w-7xl mx-auto">
-            <div className="flex items-center gap-2">
-               <Link to="/"><img src="../leadwinn.svg" alt="leadwinn logo" className='w-14 sm:w-full'/></Link>
+      <div>
+         <div className="flex justify-between items-center space-x-20 h-16 px-5 pt-10 md:max-w-5xl lg:max-w-6xl mx-auto">
+            <div className="flex justify-between items-center gap-2">
+               <Link to="/"><img src="../leadwinn.svg" alt="leadwinn logo" className='w-14 md:w-full' /></Link>
                <span className="flex flex-col text-lead-black md:max-lg:text-sm font-bold font-poppins">
                   <h1>Leadwinn</h1>
                   <h1>Academy</h1>
@@ -27,26 +28,20 @@ export const Navbar: React.FC = () => {
                <Link to="/faq">FAQ</Link>
             </div>
 
-            <div className="hidden sm:flex md:max-lg:space-x-6 space-x-8 font-poppins md:max-lg:text-sm text-base font-semibold">
+            <div className="hidden sm:flex md:max-lg:space-6 space-x-8 font-poppins md:max-lg:text-sm text-base font-semibold">
                <Link to="" className='flex justify-center items-center text-head-blue'>Sign In</Link>
                <Link to="" className="flex justify-center items-center md:max-lg:w-36 md:max-lg:h-10 w-48 h-14 gap-3 bg-head-blue text-white rounded-3xl">Enroll Now</Link>
             </div>
 
-            <div onClick={toggleFunc}
-               className="cursor">
-               {
-               nav ? <AiOutlineClose size={28}
-                  className="sm:hidden"/> : <AiOutlineMenu size={28}
-                  className="sm:hidden"/>
-               }
+            <div onClick={toggleFunc} className="cursor-pointer md:hidden">
+               {nav ? <AiOutlineClose size={28} className="sm:hidden" /> : <AiOutlineMenu size={28} className="sm:hidden" />}
             </div>
+         </div>
 
-            <div className={
-               nav ? "fixed left-0 top-0 w-1/2 h-full bg-nav-blue border-r border-gray-200 sm:hidden transition-all ease-in-out duration-500 z-10" : "fixed -left-full"
-            }>
+         <div className={nav ? "fixed left-0 top-0 w-1/2 h-full bg-nav-blue border-r border-gray-200 sm:hidden transition-all ease-in-out duration-500 z-10" : "fixed -left-full"}>
                <div className="font-poppins pt-20 mx-3 flex flex-col font-semibold">
                   <div className="flex items-center gap-2 ml-2 -mt-15">
-                     <Link to="/"><img src="../leadwinn.svg" alt="leadwinn logo" className='w-14 sm:w-full'/></Link>
+                     <Link to="/"><img src="../leadwinn.svg" alt="leadwinn logo" className='w-14 sm:w-full' /></Link>
                      <span className="flex flex-col text-lead-black font-bold font-poppins">
                         <h1>Leadwinn</h1>
                         <h1>Academy</h1>
@@ -61,7 +56,7 @@ export const Navbar: React.FC = () => {
                   <Link to="register" className="p-4 text-head-blue">Enroll Now</Link>
                </div>
             </div>
-         </nav>
-      </>
-   )
-}
+         
+      </div>
+   );
+};
