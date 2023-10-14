6c5e893caef499c81daa44518c8ddc069f297461
unknown <Adeleyeha.17@protonmail.com>
2023-10-14 23:33:03 +0100

    updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -20,7 +20,7 @@ export const Footer = () => {
 
    return (
     <div className="bg-footer-blue text-white font-poppins ">
-      <div className="grid grid-flow-row sm:grid-flow-col gap-6 lg:gap-0  py-16 px-6 md:py-24 md:max-xl:px-18 xl:px-28 lg:max-xl:px-10 lg:space-x-20 md:max-w-6xl lg:max-w-7xl mx-auto ">
+      <div className="grid grid-flow-row sm:max-md:col-span-2 md:grid-flow-col gap-6 lg:gap-0  py-16 px-6 md:py-24 md:max-xl:px-18 xl:px-28 lg:max-xl:px-10 lg:space-x-20 md:max-w-6xl lg:max-w-7xl mx-auto ">
 
          <div className="flex flex-col gap-2">
             <h1 className="font-bold text-lg">Contact Us</h1>
