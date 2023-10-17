1ec5f86f8aeff30c60a910adce8c8d8ab7a6ca64
unknown <Adeleyeha.17@protonmail.com>
2023-10-17 15:53:01 +0100

    updated home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ export const Home = () => {
          </svg>
 
          <div className="flex flex-col md:flex-row mt-14 md:mt-24 justify-between lg:max-xl:px-10 lg:space-x-20 sm:p-20 px-5 md:max-w-6xl lg:max-w-7xl mx-auto">
-            <div className="font-poppins text-lead-black px-25 sm:px-0">
+            <div className="font-poppins text-lead-black">
 
                <h1 className="text-3xl sm:text-4xl md:text-myXL xl:text-6xl font-bold md:leading-snug mb-2 md:mb-5">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span> Online Coding Classes</h1>
                <h1 className="mb-3 md:mb-5 text-sm md:text-base">Elevate Learning with Online Coding Classes Empowering Kids and  <br className="hidden sm:flex"/> Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
