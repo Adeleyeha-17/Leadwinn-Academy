494329672aca2a62d3cef247ad743d7067aa9bdb
unknown <Adeleyeha.17@protonmail.com>
2023-10-20 00:48:23 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -31,17 +31,19 @@ export const Hero: React.FC<HomeProps> = () => {
 
   return (
     <div>
-      <div className="flex flex-col md:flex-row justify-between lg:space-x-20 py-10 md:py-20 px-5 lg:px-0 xl:px-36 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto">
+      <div className="flex flex-col md:flex-row justify-between lg:space-x-20 py-10 md:py-20 px-5 lg:px-0 xl:px-46 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto">
         <div className="font-poppins text-lead-black">
 
           <h1 className="text-3xl sm:text-4xl md:text-myXL xl:text-head font-semibold xl:font-bold md:max-xl:leading-snug xl:leading-heading mb-6 md:mb-5 md:max-lg:w-[450px] xl:w-[689px]">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
-          <h1 className="xl:w-[570px] md:max-lg:w-[400px] mb-6 md:mb-5 lg:pr-20 text-sm md:text-base xl:text-lg">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
+          <h1 className="xl:w-[570px] md:w-[400px] mb-6 md:mb-5 lg:pr-20 text-sm md:text-base xl:text-lg">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
 
           <Button path="" title="Discover Course" />
 
         </div>
+    <div className="w-full">
 
-        <img src="./homeIllustration.svg" alt="home illustration" className=" sm:w-3/4 md:w-2/5 xl:w-auto xl:absoulte xl:right-32"/>
+        <img src="./homeIllustration.svg" alt="home illustration" className=" sm:w-3/4 md:w-full"/>
+    </div>
       </div>
 
       <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-20 md:h-56 xl:h-72 w-4/5 xl:w-4/6 rounded-3xl font-poppins text-white md:font-bold text-xs md:text-sm lg:text-base xl:text-xl items-center justify-between px-10 mt-3 xl:mt-16 mx-auto hidden md:flex md:mb-36">
