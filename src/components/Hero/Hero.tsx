260afbf71efc71eebe8de80eb68e97d6943f8c49
unknown <Adeleyeha.17@protonmail.com>
2023-10-19 18:42:40 +0100

    updated Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -35,7 +35,7 @@ export const Hero: React.FC<HomeProps> = () => {
         <div className="font-poppins text-lead-black">
 
           <h1 className="text-3xl sm:text-4xl md:text-myXL xl:text-7xl font-semibold md:max-xl:leading-snug xl:leading-heading mb-2 md:mb-5">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
-          <h1 className="mb-3 md:mb-5 text-sm md:max-lg:text-base xl:text-xl">Elevate Learning with Online Coding Classes Empowering Kids and  <br className="hidden sm:flex" /> Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
+          <h1 className="mb-3 md:mb-5 lg:pr-20 text-sm md:max-lg:text-base lg:text-base xl:text-2xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
 
           <Button path="" title="Discover Course" />
 
@@ -44,7 +44,7 @@ export const Hero: React.FC<HomeProps> = () => {
         <img src="./homeIllustration.svg" alt="home illustration" className="mx-auto sm:w-3/4 md:w-2/5" />
       </div>
 
-      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-20 md:h-48 xl:h-72 w-3/5 xl:w-3/4 rounded-3xl font-poppins text-white md:font-bold text-xs lg:text-base xl:text-2xl items-center justify-between px-10 -mt-10 xl:mt-16 mx-auto hidden md:flex">
+      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-20 md:h-48 xl:h-72 w-4/5 xl:w-3/4 rounded-3xl font-poppins text-white md:font-bold text-xs md:text-sm lg:text-base xl:text-2xl items-center justify-between px-10 -mt-10 xl:mt-16 mx-auto hidden md:flex">
         <div className="flex flex-col items-center mx-auto">
           <img src="./edu1.svg" alt="" className="mx-auto xl:mb-6" />
           <h1>Over 100+ Students</h1>
