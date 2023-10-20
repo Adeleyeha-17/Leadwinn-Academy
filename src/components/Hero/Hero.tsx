fadaf25979e9b3dab5a051701ba2096815a25838
unknown <Adeleyeha.17@protonmail.com>
2023-10-20 08:51:39 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -34,7 +34,7 @@ export const Hero: React.FC<HomeProps> = () => {
       <div className="flex flex-col md:flex-row justify-between lg:space-x-20 py-10 md:py-20 px-5 lg:px-0 xl:px-36 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto">
         <div className="font-poppins text-lead-black">
 
-          <h1 className="text-3xl sm:text-4xl xl:text-head font-semibold xl:font-bold md:max-xl:leading-snug xl:leading-heading mb-6 md:mb-5 md:w-[400px] xl:w-[689px]">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
+          <h1 className="text-3xl sm:text-4xl xl:text-head font-semibold xl:font-bold md:max-xl:leading-snug xl:leading-heading mb-6 md:mb-5 md:w-[370px] lg:w-[400px] xl:w-[689px]">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
           <h1 className="xl:w-[500px] md:max-lg:w-[420px] mb-6 md:mb-5 text-sm md:text-base lg:text-lg xl:text-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
 
           <Button path="" title="Discover Course" />
@@ -46,7 +46,7 @@ export const Hero: React.FC<HomeProps> = () => {
     </div>
       </div>
 
-      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-20 md:h-56 xl:h-72 w-4/5 xl:w-4/6 rounded-3xl font-poppins text-white md:font-bold text-xs md:text-sm lg:text-base xl:text-xl items-center justify-between px-10 mt-3 lg:mt-5 xl:mt-6 mx-auto hidden md:flex md:mb-36">
+      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-20 md:h-56 xl:h-72 w-4/5 xl:w-4/6 rounded-3xl font-poppins text-white md:font-bold text-xs md:text-sm lg:text-base xl:text-xl items-center justify-between px-10 mt-3 md:-mt-8 lg:mt-5 xl:mt-6 mx-auto hidden md:flex md:mb-36">
         <div className="flex flex-col items-center mx-auto">
           <img src="./edu1.svg" alt="" className="mx-auto xl:mb-6" />
           <h1>Over 100+ Students</h1>
