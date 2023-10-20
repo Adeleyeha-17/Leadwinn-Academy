9a6f4d8357c4dc18dd1152f638577f04cff9929e
unknown <Adeleyeha.17@protonmail.com>
2023-10-20 18:17:54 +0100

    updated Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -32,16 +32,12 @@ export const Hero: React.FC<HomeProps> = () => {
   return (
     <div>
       <div className="flex flex-col sm:flex-row justify-between lg:space-x-20 py-10 sm:max-md:px-20 md:py-20 px-5 lg:px-0 xl:px-36 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto">
-        <div className="font-poppins text-lead-black w-1/2">
-
-          <h1 className="text-3xl sm:text-6xl font-semibold md:leading-snug mb-6 md:mb-5 sm:max-md:w-[420px] lg:w-[650px] 2xl:w-[790px]">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
-          <h1 className="xl:w-[500px] md:w-[420px] sm:max-md:w-[350px] mb-6 md:mb-5 text-sm md:text-base">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
-
+        <div className="font-poppins text-lead-black w-4/5">
+          <h1 className="text-3xl sm:text-6xl font-semibold mb-6 max-w-4xl">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
+          <h1 className=" mb-6 text-sm md:text-base">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
           <Button path="" title="Discover Course" />
-
         </div>
-    <div className="w-1/2">
-
+    <div className="w-2/5">
         <img src="./homeIllustration.svg" alt="home illustration" className=" mx-auto w-3/4"/>
     </div>
       </div>
