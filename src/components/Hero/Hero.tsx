7da2013da9f66f127d310ff21c071ad44c044a83
unknown <Adeleyeha.17@protonmail.com>
2023-10-20 19:13:08 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -34,7 +34,7 @@ export const Hero: React.FC<HomeProps> = () => {
       <div className="flex flex-col sm:flex-row justify-between lg:space-x-20 py-10 sm:max-md:px-20 md:py-20 px-5 lg:px-0 xl:px-36 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto">
         <div className="font-poppins text-lead-black w-3/5">
           <h1 className="text-3xl sm:text-head font-semibold mb-6 leading-normal md:max-w-3xl ">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
-          <h1 className=" mb-6 text-sm md:text-base lg:text-lg md:max-w-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
+          <h1 className=" mb-6 text-sm md:text-base md:max-w-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
           <Button path="" title="Discover Course" />
         </div>
     <div className="w-2/5">
@@ -42,7 +42,7 @@ export const Hero: React.FC<HomeProps> = () => {
     </div>
       </div>
 
-      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-5/6 md:h-52 w-1/2 sm:w-2/3 rounded-3xl font-poppins text-white md:font-bold text-xs md:text-sm items-center justify-between mt-14 sm:mt-3 md:-mt-8 lg:-mt-7 xl:mt-6 mx-auto sm:flex mb-28 sm:mb-36">
+      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-5/6 md:h-52 w-1/2 sm:w-2/3 rounded-3xl font-poppins text-white md:font-semibold text-xs md:text-sm lg:text-lg items-center justify-between mt-14 sm:mt-3 md:-mt-8 lg:-mt-7 xl:mt-6 mx-auto sm:flex mb-28 sm:mb-36">
         <div className="flex flex-col items-center mx-auto">
           <img src="./edu1.svg" alt="" className="mx-auto mb-3 w-3/4" />
           <h1>Over 100+ Students</h1>
