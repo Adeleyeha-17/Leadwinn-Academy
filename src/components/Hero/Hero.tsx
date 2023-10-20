7c03b1bffe845c7498ca96dbbee2cd220cd33d5d
unknown <Adeleyeha.17@protonmail.com>
2023-10-20 08:27:10 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -35,7 +35,7 @@ export const Hero: React.FC<HomeProps> = () => {
         <div className="font-poppins text-lead-black">
 
           <h1 className="text-3xl sm:text-4xl xl:text-head font-semibold xl:font-bold md:max-xl:leading-snug xl:leading-heading mb-6 md:mb-5 md:w-[400px] xl:w-[689px]">Engage in the Future of Learning with <span className={`animate-slide-top ${currentColorClass}`}>{currentWord}</span>  Online Coding Classes</h1>
-          <h1 className="xl:w-[500px] md:max-lg:w-[420px] mb-6 md:mb-5 text-sm md:text-base xl:text-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
+          <h1 className="xl:w-[500px] md:max-lg:w-[420px] mb-6 md:mb-5 text-sm md:text-base lg:text-lg xl:text-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
 
           <Button path="" title="Discover Course" />
 
