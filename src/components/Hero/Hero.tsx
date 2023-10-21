005cdc160f1eb0f818c96ffdede14dce9b6d2bc6
unknown <Adeleyeha.17@protonmail.com>
2023-10-21 09:52:04 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -44,21 +44,21 @@ export const Hero: React.FC<HomeProps> = () => {
 
       <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-5/6 sm:h-3/5 md:h-64 w-1/2 sm:w-4/5 md:w-3/4 rounded-3xl font-poppins text-white font-semibold text-sm lg:text-lg items-center justify-between mt-14 mx-auto sm:flex mb-28 py-3 sm:max-md:px-10 md:py-10 sm:py-0 sm:mb-36">
         <div className="flex flex-col mb-12 items-center mx-auto">
-          <img src="./edu1.svg" alt="" className="mx-auto mb-4 w-28 sm:w-3/4" />
+          <img src="./edu1.svg" alt="" className="mx-auto mb-4 w-28 md:w-3/4" />
           <h1>Over 100+ Students</h1>
         </div>
 
         <div className="w-20 m-8 border-gray-200 border-2 transform rotate-90 mx-auto rounded-lg"></div>
 
         <div className="flex flex-col mb-14 items-center justify-center mx-auto">
-          <img src="./edu2.svg" alt="" className="mx-auto mb-4 w-28 sm:w-3/4" />
+          <img src="./edu2.svg" alt="" className="mx-auto mb-4 w-28 md:w-3/4" />
           <h1>Over 100+ Certified</h1>
         </div>
 
         <div className="w-20 m-12 border-gray-200 border-2 transform rotate-90 mx-auto rounded-lg"></div>
 
         <div className="flex flex-col mb-12 items-center mx-auto">
-          <img src="./edu3.svg" alt="" className="mx-auto mb-4 w-28 sm:w-3/4" />
+          <img src="./edu3.svg" alt="" className="mx-auto mb-4 w-28 md:w-3/4" />
           <h1>Over 10+ Countries</h1>
         </div>
       </div>
