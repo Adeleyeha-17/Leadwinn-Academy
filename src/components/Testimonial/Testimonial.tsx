5a9750024595e8c2048571744bd825d9f88561d5
unknown <Adeleyeha.17@protonmail.com>
2023-10-23 10:08:20 +0100

    Updated Hero section

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -6,69 +6,79 @@ export const Testimonial = () => {
 
    const testimonials = [
       {
-        name: 'Mrs Enaworu',
-        message: 'I like the idea that my son is interested in the course and looks forward to the weekly sessions. The Tutor is also very patient with the kids and ensure that the kids are not distracted but stayed focused on the session',
+         name: 'Mrs Enaworu',
+         message: 'I like the idea that my son is interested in the course and looks forward to the weekly sessions. The Tutor is also very patient with the kids and ensure that the kids are not distracted but stayed focused on the session'
       },
       {
          name: 'Mrs Agbetosin',
-         message: 'My child has learnt a lot of things in a very short period. The patient and dedication of the organization and tutor are amazing',
-       }, {
+         message: 'My child has learnt a lot of things in a very short period. The patient and dedication of the organization and tutor are amazing'
+      },
+      {
          name: 'Mrs Suzanne O.',
-         message: 'My daughter enjoyed each session and was always looking forward to the next class',
-       },
-       {
+         message: 'My daughter enjoyed each session and was always looking forward to the next class'
+      },
+      {
          name: 'Mrs Adelopo E.',
-         message: 'The classes are highly engaging. Teacher is very patient and loves to carry the students along. My son is always looking forward to the next class. It is great to see him channel his skills in a productive way',
-       },{
+         message: 'The classes are highly engaging. Teacher is very patient and loves to carry the students along. My son is always looking forward to the next class. It is great to see him channel his skills in a productive way'
+      }, {
          name: 'Mrs Obaro',
-         message: 'It taught my son how to be independent and create game by himself which made him really happy',
-       },{
+         message: 'It taught my son how to be independent and create game by himself which made him really happy'
+      }, {
          name: 'Mrs Abimbola 0.',
-         message: 'Great Academy where kids can learn to make games independently',
-       },
-    ];
+         message: 'Great Academy where kids can learn to make games independently'
+      },
+   ];
 
-    const sliderSettings = {
-  dots: true,
-  infinite: true,
-  speed: 2000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  pauseOnHover: true,
-  pauseOnFocus: true,
-  cssEase: "linear"
+   const sliderSettings = {
+      dots: true,
+      infinite: true,
+      speed: 2000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      cssEase: "linear"
 
 
-};
+   };
 
-  return (
-    <div className="bg-gradient-to-tl from-test-gra-2 to-test-gra h-[200px] md:h-[320px] lg:h-[440px] xl:h-[550px] font-poppins flex flex-col justify-center p-36 text-white overflow-hidden ">
+   return (
+      <div className="bg-gradient-to-tl from-test-gra-2 to-test-gra h-[200px] md:h-[320px] lg:h-[440px] xl:h-[550px] font-poppins flex flex-col md:justify-center max-sm:px-10 max-sm:py-8 md:p-36 text-white overflow-hidden ">
 
-<div>
-      <h1 className="md:text-base lg:text-2xl xl:text-4xl font-bold mb-6 ">What are the parents saying?</h1>
-      </div>
-      
-      <div className="grid grid-cols-2 items-center mb-10">
          <div>
-         <img src="./sliderImg.svg" alt="slider Image" className="md:w-4/5"/>
-
+            <h1 className="text-xs md:text-base lg:text-2xl font-bold mb-3 md:mb-6 ">What are the parents saying?</h1>
          </div>
 
-      <div className='bg-white font-poppins bg-opacity-10 rounded-lg h-52 lg:h-72 xl:h-96 w-full flex items-center justify-center md:max-lg:-ml-20 lg:max-xl:-ml-36 xl:-ml-52 lg:pt-20'>
-      <Slider {...sliderSettings} className='w-full lg:w-4/5'>
-      {testimonials.map((testimonial, index) => (
-        <div key={index} className='md:py-6 md:max-lg:px-4'>
-          <p className="md:text-base xl:text-xl md:mb-3 lg:mb-12 flex items-center justify-center">{`"${testimonial.message}"`}</p>
-          <h3 className="xl:text-xl xl:pt-12 flex items-center justify-center">{testimonial.name}</h3>
-        </div>
-      ))}
-    </Slider>
+         <div className="grid grid-cols-2 items-center mb-10">
+            <div>
+               <img src="./sliderImg.svg" alt="slider Image" className="hidden md:flex md:w-4/5"/>
+            </div>
+
+            <div className='bg-white font-poppins bg-opacity-10 rounded-lg h-32 md:h-40 lg:h-52 xl:h-72 w-full flex items-center justify-center md:max-lg:-ml-20 lg:max-xl:-ml-24 xl:-ml-40'>
+               <Slider {...sliderSettings} className='w-full  lg:w-4/5'>
+                  {
+                  testimonials.map((testimonial, index) => (
+                     <div key={index}
+                        className='md:py-2 lg:py-3 xl:py-5'>
+                        <p className="text-xs md:text-sm lg:text-base xl:text-lg md:mb lg:mb-12 flex items-center justify-center">
+                           {
+                           `"${
+                              testimonial.message
+                           }"`
+                        }</p>
+                        <h3 className="text-xs md:text-sm lg:text-base xl:text-lg xl:pt-12 flex items-center justify-center">
+                           {
+                           testimonial.name
+                        }</h3>
+                     </div>
+                  ))
+               } </Slider>
+            </div>
+         </div>
       </div>
-      </div>
-      </div>
-   
-  )
+
+   )
 }
