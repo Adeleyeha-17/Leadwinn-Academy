42ad5d846bd3b784492d08cca6dd6b19e1d82bc2
unknown <Adeleyeha.17@protonmail.com>
2023-10-23 10:40:29 +0100

    updated testimonials

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -46,24 +46,24 @@ export const Testimonial = () => {
    };
 
    return (
-      <div className="bg-gradient-to-tl from-test-gra-2 to-test-gra h-[200px] md:h-[320px] lg:h-[440px] xl:h-[550px] font-poppins flex flex-col md:justify-center max-sm:px-10 max-sm:py-8 md:p-36 text-white overflow-hidden ">
+      <div className="bg-gradient-to-tl from-test-gra-2 to-test-gra h-[270px] md:h-[320px] lg:h-[440px] xl:h-[550px] font-poppins md:flex flex-col md:justify-center max-sm:px-10 max-sm:py-8 md:p-36 text-white overflow-hidden ">
 
          <div>
             <h1 className="text-xs md:text-base lg:text-2xl font-bold mb-3 md:mb-6 ">What are the parents saying?</h1>
          </div>
 
          <div className="grid grid-cols-2 items-center mb-10">
-            <div>
-               <img src="./sliderImg.svg" alt="slider Image" className="hidden md:flex md:w-4/5"/>
+            <div className='hidden md:flex'>
+               <img src="./sliderImg.svg" alt="slider Image" className="md:w-4/5"/>
             </div>
 
-            <div className='bg-white font-poppins bg-opacity-10 rounded-lg h-32 md:h-40 lg:h-52 xl:h-72 w-full flex items-center justify-center md:-ml-10 lg:-ml-24 xl:-ml-40'>
+            <div className='bg-white font-poppins bg-opacity-10 rounded-lg h-32 md:h-40 lg:h-52 xl:h-72 max-sm:w-72 max-sm:px-5 max-sm:py- flex items-center justify-center md:-ml-10 lg:-ml-24 xl:-ml-40'>
                <Slider {...sliderSettings} className='w-full lg:w-4/5'>
                   {
                   testimonials.map((testimonial, index) => (
                      <div key={index}
-                        className='md:py-2 xl:py-5'>
-                        <p className="text-xs lg:text-sm xl:text-base md:mb lg:mb-12 flex items-center justify-center">
+                        className='py-2 xl:py-5'>
+                        <p className="text-xs lg:text-sm xl:text-base max-sm:mb-3 lg:mb-12 flex items-center justify-center">
                            {
                            `"${
                               testimonial.message
