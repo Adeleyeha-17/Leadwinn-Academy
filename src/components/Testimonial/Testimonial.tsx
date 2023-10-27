27e5b3353f660b186b8c6c2d1e0f2252237c7430
unknown <Adeleyeha.17@protonmail.com>
2023-10-27 13:35:06 +0100

    updated Testimonial

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -46,10 +46,19 @@ export const Testimonial = () => {
    };
 
    return (
-      <div className="bg-gradient-to-tl from-test-gra-2 to-test-gra md:h-[340px] lg:h-[440px] xl:h-4/5 font-poppins md:flex flex-col md:justify-center max-sm:px-10 max-sm:py-8 md:p-28 text-white overflow-hidden ">
+      <div className="relative bg-gradient-to-tl from-test-gra-2 to-test-gra md:h-[340px] lg:h-[440px] xl:h-4/5 font-poppins md:flex flex-col md:justify-center max-sm:px-10 max-sm:py-8 md:p-28 text-white overflow-hidden ">
+         <div className='rounded-full w-12 h-12 md:bg-white md:bg-opacity-5 absolute top-6'></div>
+         <div className='rounded-full w-14 h-14 md:bg-white md:bg-opacity-5 absolute right-9 bottom-28'></div>
+         <div className='rounded-full w-40 h-40 md:bg-white md:bg-opacity-5 absolute top-40 left-[600px]'></div>
+         <div className='rounded-full w-32 h-32 md:bg-white md:bg-opacity-5 absolute bottom-20 left-18'></div>
+         <div className='rounded-full w-28 h-28 md:bg-white md:bg-opacity-5 absolute top-32 right-40'></div>
+         <div className='rounded-full w-32 h-32 md:bg-white md:bg-opacity-5 absolute right-40 bottom-4'></div>
+         <div className='rounded-full w-20 h-20 md:bg-white md:bg-opacity-5 absolute top-62 left-96 right-96'></div>
+         <div className='rounded-full w-20 h-20 md:bg-white md:bg-opacity-5 absolute left-52 top-40'></div>
+         <div className='rounded-full w-10 h-10 md:bg-white md:bg-opacity-5 absolute top-6 right-12'></div>
 
          <div>
-            <h1 className="text-sm md:text-base lg:text-2xl font-bold mb-3 md:mb-6 max-md:text-center">What are the parents saying?</h1>
+            <h1 className="text-sm md:text-base lg:text-2xl font-bold mb-3 md:mb-6 max-md:text-center z-10">What are the parents saying?</h1>
          </div>
 
          <div className="grid md:grid-cols-2 items-center mb-10">
@@ -57,7 +66,7 @@ export const Testimonial = () => {
                <img src="./sliderImg.svg" alt="slider Image" className="w-4/5"/>
             </div>
 
-            <div className='md:bg-white font-poppins md:bg-opacity-10 rounded-lg h-32 md:h-40 lg:h-52 xl:h-72 max-sm:w-72 max-lg:px-5 max-sm:py- flex items-center justify-center md:-ml-12 lg:-ml-28 xl:-ml-36'>
+            <div className='md:bg-white font-poppins md:bg-opacity-30 rounded-lg h-32 md:h-40 lg:h-52 xl:h-72 max-sm:w-72 max-lg:px-5 max-sm:py- flex items-center justify-center md:-ml-12 lg:-ml-28 xl:-ml-36'>
                <Slider {...sliderSettings} className='w-full lg:w-4/5'>
                   {
                   testimonials.map((testimonial, index) => (
