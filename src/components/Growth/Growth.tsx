ecdfe496d7f1f73bb1bae790a5c3e5ce0da12b05
unknown <Adeleyeha.17@protonmail.com>
2023-10-30 12:39:18 +0100

    updated growth

--- src/components/Growth/Growth.tsx
+++ src/components/Growth/Growth.tsx
@@ -2,16 +2,16 @@ import Button from "../Button"
 
 export const Growth = () => {
   return (
-   <div className="flex flex-col md:flex-row justify-between lg:space-x-20 py-10 sm:px-20 md:py-20 px-5 lg:px-0 xl:px-28 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto">
-    <div className="flex max-sm:flex-col justify-between items-center w-full gap-20">
-      <div className="flex flex-col gap-6 text-head-black w-2/5">
+   <div className="flex flex-col md:flex-row justify-between lg:space-x-20 py-10 sm:px-20 md:py-20 px-5 lg:px-0 xl:px-28 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto bg-[#F4F7FA]">
+    <div className="flex max-sm:flex-col justify-between items-center w-full">
+      <div className="flex flex-col gap-6 text-head-black w-3/5">
          <h1 className=" font-bold text-xl sm:text-4xl max-w-[250px] sm:max-w-[420px] max-sm:capitalize">See our growth, progress, and <span className="text-hero-blue">evolution</span></h1>
          <h4 className="text-base max-w-[400px]">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
 
          <Button path="" title="LEARN MORE"/>
       </div>
 
-      <div className="w-3/5">
+      <div className="w-3/6">
          <img src="./growth.svg" alt="growth image" className="w-full"/>
       </div>
     </div>
