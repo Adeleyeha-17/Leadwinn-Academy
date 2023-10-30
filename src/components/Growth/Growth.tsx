779597b305a7ae0857ba8256f1b291e3dc62d2f0
unknown <Adeleyeha.17@protonmail.com>
2023-10-30 12:06:57 +0100

    updated

--- src/components/Growth/Growth.tsx
+++ src/components/Growth/Growth.tsx
@@ -3,7 +3,7 @@ import Button from "../Button"
 export const Growth = () => {
   return (
     <div className="flex justify-center items-center gap-14">
-      <div className="flex flex-col gap-6 text-head-black max-w-[320px]">
+      <div className="flex flex-col gap-6 text-head-black max-w-[420px]">
          <h1 className=" font-bold text-4xl">See our growth, progress, and <span className="text-hero-blue">evolution</span></h1>
          <h4 className="text-base">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
 
