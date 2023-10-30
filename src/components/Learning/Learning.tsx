51c377217511ba70c73567af81f0706ad0c5e931
unknown <Adeleyeha.17@protonmail.com>
2023-10-30 11:37:43 +0100

    updated learning

--- src/components/Learning/Learning.tsx
+++ src/components/Learning/Learning.tsx
@@ -3,7 +3,7 @@ import Button from "../Button"
 export const Learning = () => {
   return (
     <div className="mb-20">
-      <div className="bg-learning bg-contain bg-no-repeat py-52">
+      <div className="bg-learning bg-contain bg-no-repeat py-40">
          <div className="flex justify-center items-center flex-col">
          <h1>Start your learning journey today!</h1>
          <h4>Start Your Learning Journey Today and Discover a World of Opportunities Await</h4>
