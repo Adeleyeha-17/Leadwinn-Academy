62212412df395344c5fb5668d2f050b6fd924840
unknown <Adeleyeha.17@protonmail.com>
2023-10-30 13:40:51 +0100

    updated growth

--- src/components/Growth/Growth.tsx
+++ src/components/Growth/Growth.tsx
@@ -6,7 +6,7 @@ export const Growth = () => {
     <div className="flex max-sm:flex-col justify-between items-center w-full">
       <div className="flex flex-col gap-4 lg:gap-6 text-head-black md:w-3/5 max-sm:mb-10">
          <h1 className=" font-bold text-2xl lg:text-4xl max-w-[330px] lg:max-w-[440px] max-sm:capitalize">See our growth, progress, and <span className="text-hero-blue">evolution</span></h1>
-         <h4 className="text-[13px] lg:text-base max-w-[390px] lg:max-w-[445px]">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
+         <h4 className="text-[13px] lg:text-base max-w-[370px] lg:max-w-[445px]">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
 
          <Link to="" className="flex justify-center items-center sm:max-md:w-28 sm:max-md:h-10 md:w-36 md:h-10 w-24 h-8 gap-3 bg-head-blue text-white text-xs sm:text-sm sm:font-normal rounded-3xl">LEARN MORE</Link>
 
