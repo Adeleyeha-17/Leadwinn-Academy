de3f29d0ef15c99e98f7aa34160a1219e3cc1c7c
unknown <Adeleyeha.17@protonmail.com>
2023-11-03 14:34:16 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -36,7 +36,7 @@ export const Hero: React.FC<HomeProps> = () => {
           <h1 className="max-sm:text-[25px] sm:max-lg:text-[30px] lg:max-xl:text-[37px] xl:max-2xl:text-[40.5px] 2xl:text-[60px] font-bold mb-6 leading-normal max-w-md sm:mx-0 sm:max-w-[400px] lg:max-2xl:max-w-[540px] 2xl:max-w-[820px]">Engage in the Future of Learning with <span className={`${currentColorClass}`}>
   {currentWord}
 </span>  Online Coding Classes</h1>
-          <h1 className="hidden md:flex mb-6 text-xs lg:text-base md:max-w-md lg:max-w-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
+          <h1 className="hidden md:flex mb-6 text-xs lg:text-base md:max-w-[430px] lg:max-w-xl">Elevate Learning with Online Coding Classes Empowering Kids and Teens Aged 6-17 to Shape the Future through Design and Innovation.</h1>
           <Button path="" title="Discover Course" />
         </div>
     <div className="md:w-2/5">
