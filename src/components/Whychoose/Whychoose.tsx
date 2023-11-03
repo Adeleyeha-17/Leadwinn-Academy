f92afe5573015c727dce21ebf9893d3b43347fa0
unknown <Adeleyeha.17@protonmail.com>
2023-11-03 22:42:01 +0100

    updated

--- src/components/Whychoose/Whychoose.tsx
+++ src/components/Whychoose/Whychoose.tsx
@@ -4,10 +4,10 @@ export const Whychoose = () => {
 
   return (
     
-  <div className="py-10 md:px-12 md:py-20 px-5 xl:px-28 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto text-poppins">
+  <div className="py-10 md:px-12 md:py-20 px-5 xl:px-28 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto font-poppins">
       <div>
         <h2 className="text-head-black font-bold max-sm:text-xl text-[40px] leading-[48px] sm:mb-3">Why Choose <span className="text-hero-blue">Leadwinn?</span></h2>
-        <h3 className="max-sm:text-xs text-base xl:text-lg text-head-black max-w-[280px] sm:max-w-[410px] lg:max-w-[350px]">At Leadwinn, we're dedicated to providing the best online coding education for kids and teens. Here are the compelling reasons why you should choose us</h3>
+        <h3 className="max-sm:text-xs text-base xl:text-lg text-head-black max-w-[320px] sm:max-w-[410px]">At Leadwinn, we're dedicated to providing the best online coding education for kids and teens. Here are the compelling reasons why you should choose us</h3>
       </div>
      </div>
 )}
