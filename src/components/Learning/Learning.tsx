9cd295651333fd0869fc5d4f90a287c4179ab0c6
unknown <Adeleyeha.17@protonmail.com>
2023-11-03 21:31:25 +0100

    updated

--- src/components/Learning/Learning.tsx
+++ src/components/Learning/Learning.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export const Learning = () => {
   return (
-    <div className="bg-learning-mobile sm:bg-learning bg-contain bg-no-repeat flex items-center justify-center h-[260px] lg:h-[320px] xl:h-[400px] 2xl:h-[560px] font-poppins max-sm:-mb-28 sm:-mb-20 lg:-mb-10">
+    <div className="bg-learning-mobile sm:bg-learning bg-contain bg-no-repeat flex items-center justify-center h-[260px] lg:h-[320px] xl:h-[400px] 2xl:h-[560px] font-poppins max-sm:-mb-32 sm:-mb-20 lg:-mb-10">
       <div className="flex flex-col gap-1 sm:gap-2 items-center justify-center text-center text-head-black -mt-32 sm:max-md:-mt-16 xl:-mt-6 ">
       <h1 className="font-bold text-sm sm:text-xl lg:text-[32px] lg:leading-[44px] lg:mb-3">Start your learning journey today!</h1>
       <h1 className="text-xs lg:text-base mb-2 sm:mb-5">Start Your Learning Journey and Discover a World of Opportunities Await</h1>
