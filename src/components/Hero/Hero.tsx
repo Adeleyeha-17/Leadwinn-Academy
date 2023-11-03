6627da6e7453d4f2ed30b43286e1f2f5f1b796d2
unknown <Adeleyeha.17@protonmail.com>
2023-11-03 22:46:00 +0100

    updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -44,7 +44,7 @@ export const Hero: React.FC<HomeProps> = () => {
     </div>
       </div>
 
-      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-2/5 sm:max-lg:h-[160px] sm:max-lg:w-[650px] lg:h-[184] w-2/5 lg:w-[811px] rounded-[50px] font-poppins text-white font-semibold text-xs sm:text-[9px] lg:text-sm mx-auto max-sm:mt-16 py-3 sm:px-6 xl:mt-32">
+      <div className=" bg-gradient-to-tr from-home-gra-2 to-home-gra h-2/5 sm:max-lg:h-[160px] sm:max-lg:w-[650px] lg:h-[184] w-1/2 lg:w-[811px] rounded-[50px] font-poppins text-white font-semibold text-sm sm:text-[9px] lg:text-sm mx-auto max-sm:mt-16 py-3 sm:px-6 xl:mt-32">
         <div className="items-center justify-between sm:flex mt-5 sm:mt-3 lg:mt-5">
 
         <div className="flex flex-col mb-10 sm:mb-12 items-center justify-center mx-auto">
