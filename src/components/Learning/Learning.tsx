50dd2b61136a7171b9e214052816a3cb1f4e9984
unknown <Adeleyeha.17@protonmail.com>
2023-11-03 16:40:29 +0100

    updated Learning component

--- src/components/Learning/Learning.tsx
+++ src/components/Learning/Learning.tsx
@@ -5,10 +5,10 @@ export const Learning = () => {
   return (
     <div className="bg-learning-mobile sm:bg-learning bg-contain bg-no-repeat flex items-center justify-center h-[200px] sm:h-[240px] lg:h-[320px] xl:h-[400px] font-poppins -mb-16 lg:-mb-10">
       <div className="flex flex-col gap-1 sm:gap-2 items-center text-center text-head-black max-sm:-mt-16">
-      <h1 className="font-bold text-xs sm:text-xl lg:text-3xl">Start your learning journey today!</h1>
-      <h1 className="text-xs sm:text-xs lg:text-base mb-5">Start Your Learning Journey and Discover a World of Opportunities Await</h1>
+      <h1 className="font-bold text-sm sm:text-xl lg:text-3xl">Start your learning journey today!</h1>
+      <h1 className="text-xs sm:text-xs lg:text-base mb-2 sm:mb-5">Start Your Learning Journey and Discover a World of Opportunities Await</h1>
 
-      <Link to="" className="flex justify-center items-center py-1 sm:py-2 lg:py-3 px-2 sm:px-4 lg:px-6 bg-head-blue text-white text-xs md:text-sm font-semibold rounded-3xl">Sign up now</Link>
+      <Link to="" className="flex justify-center items-center py-2 lg:py-3 px-4 lg:px-6 bg-head-blue text-white text-xs md:text-sm font-semibold rounded-3xl">Sign up now</Link>
 
       </div>
     </div>
