aa31e60d5be929bc40813bba36e19306350c9e24
unknown <Adeleyeha.17@protonmail.com>
2023-11-03 10:13:27 +0100

    changes made on goal component

--- src/components/Learning/Learning.tsx
+++ src/components/Learning/Learning.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export const Learning = () => {
   return (
-    <div className="bg-learning bg-contain bg-no-repeat flex items-center justify-center md:h-[220px] lg:h-[300px] xl:h-[400px] font-poppins">
+    <div className="md:bg-learning bg-contain bg-no-repeat flex items-center justify-center w-full h-[220px] lg:h-[300px] xl:h-[400px] font-poppins mx-auto">
       <div className="flex flex-col gap-2 items-center text-center text-head-black">
       <h1 className="font-bold text-3xl">Start your learning journey today!</h1>
       <h1 className="text-base mb-5">Start Your Learning Journey and Discover a World of Opportunities Await</h1>
