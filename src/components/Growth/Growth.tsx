1243d2c4c4a28e3bd634129cd1bb1f50845b5077
unknown <Adeleyeha.17@protonmail.com>
2023-11-05 11:06:26 +0100

    updated

--- src/components/Growth/Growth.tsx
+++ src/components/Growth/Growth.tsx
@@ -5,8 +5,8 @@ export const Growth = () => {
    <div className="flex flex-col md:flex-row justify-between py-10 md:px-12 md:py-20 px-5 xl:px-28 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto md:bg-[#F4F7FA] font-poppins">
     <div className="flex max-sm:flex-col justify-between items-center w-full">
       <div className="flex flex-col gap-4 lg:gap-6 text-head-black md:max-xl:w-3/5 xl:w-2/5 max-sm:mb-10">
-         <h1 className=" font-bold text-2xl sm:text-[1.875rem] lg:text-[2.5rem] sm:leading-9 lg:leading-[2.875rem] max-w-[20.625rem] sm:max-w-[23.75rem] lg:max-w-[30.625rem] max-sm:capitalize">See our growth, progress, and <span className="text-hero-blue">evolution</span></h1>
-         <h4 className="text-[0.8125rem] lg:text-base max-w-[23.125rem] lg:max-w-[27.8125rem]">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
+         <h1 className=" font-bold text-2xl sm:text-[1.875rem] lg:text-[2.5rem] sm:leading-9 lg:leading-[2.875rem] max-w-[20.625rem] sm:max-w-[23.75rem] lg:max-w-[31rem] max-sm:capitalize">See our growth, progress, and <span className="text-hero-blue">evolution</span></h1>
+         <h4 className="text-[0.8125rem] lg:text-base max-w-[23.125rem] lg:max-w-[28rem]">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
 
          <Link to="/about" className="flex justify-center items-center sm:max-md:w-28 sm:max-md:h-10 md:w-36 md:h-10 w-24 h-8 gap-3 bg-head-blue text-white font-poppins text-xs sm:text-sm sm:font-semibold rounded-3xl transition hover:bg-blue-600 ease-in-out duration-300">LEARN MORE</Link>
 
