99ebb227a48813bd506c054987df66d9648f40e5
unknown <Adeleyeha.17@protonmail.com>
2023-11-05 11:33:33 +0100

    updated

--- src/components/Growth/Growth.tsx
+++ src/components/Growth/Growth.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Growth = () => {
   return (
    <div className="flex flex-col md:flex-row justify-between py-10 md:px-12 md:py-20 px-5 xl:px-28 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl md:bg-[#F4F7FA] font-poppins">
-    <div className="flex max-sm:flex-col sm:justify-between sm:items-center sm:w-full">
+    <div className="flex max-sm:flex-col md:justify-between md:items-center sm:w-full">
       <div className="flex flex-col gap-4 lg:gap-6 text-head-black md:max-xl:w-3/5 xl:w-2/5 max-sm:mb-10">
          <h1 className=" font-bold text-2xl sm:text-[1.875rem] lg:text-[2.5rem] sm:leading-9 lg:leading-[2.875rem] max-w-[20.625rem] sm:max-w-[23.75rem] lg:max-w-[31rem] max-sm:capitalize">See our growth, progress, and <span className="text-hero-blue">evolution</span></h1>
          <h4 className="text-[0.8125rem] lg:text-base max-w-[23.125rem] lg:max-w-[28rem]">Founded in 2020, Leadwinn has trained 100+ students in 10+ countries, demonstrating a global commitment to top-notch coding education. Our mission is to shape the future of education in an evolving digital landscape.</h4>
