c45deadc41d3b8a0b70b403f298afa9538697eba
unknown <Adeleyeha.17@protonmail.com>
2023-11-09 16:37:16 +0100

    updated

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -36,17 +36,18 @@ export const Courses = () => {
          overlayImg: "./web-img.png"
       }
    ]
-
+ 
+    
   return (
-     <div className="bg-footer-blue h-[27rem] sm:h-[65rem] lg:h-[50rem] xl:h-[55rem]  font-poppins py-10 md:px-12 md:py-20 px-5 xl:px-40 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto overflow-hidden">
+     <div className="bg-footer-blue h-[30rem] sm:h-[65rem] lg:h-[50rem] xl:h-[55rem]  font-poppins py-10 md:px-12 md:py-20 px-5 xl:px-40 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto overflow-hidden">
       <div className="text-center text-white mb-16">
          <h1 className="font-bold text-xl xl:text-3xl mb-3">Explore Our Courses</h1>
          <h4 className="max-sm:text-xs text-base max-w-xs sm:max-w-2xl mx-auto">Our Microdegrees are specialized programs which upon completion will set you firmly on your chosen IT career path</h4>
       </div>
-
+      
       <div className="grid grid-cols-6 sm:grid-cols-2 lg:grid-cols-3 gap-72 sm:gap-10">
          {courses.map((data, index) => (
-            <div key={index} className="w-[17rem] sm:w-[17rem] xl:w-[21rem] relative rounded-3xl overflow-hidden mx-auto">
+            <div key={index} className="w-[17rem] sm:w-[17rem] xl:w-[21rem] relative rounded-3xl overflow-hidden mx-auto course-card">
                <img src={data.img} alt={data.heading} className="w-full rounded-xl" />
                <div className="absolute translate-y-40 xl:translate-y-52 hover:translate-y-0 transition ease-in-out duration-680 flex flex-col justify-center items-center gap-10 bg-courses bg-opacity-70 backdrop-blur-sm inset-0 text-white rounded-3xl rounded-t-none">
                   <h1 className="font-bold text-xs sm:text-base xl:text-xl">{data.heading}</h1>
