44689603cd1b857a182f96ae3feacc25aa042453
unknown <Adeleyeha.17@protonmail.com>
2023-11-14 16:19:27 +0100

    updated

--- src/components/Innovators/Innovators.tsx
+++ src/components/Innovators/Innovators.tsx
@@ -2,7 +2,9 @@
 export const Innovators:React.FC = () => {
   return (
     <div>
-      <div className="bg-footer-blue flex flex-col justify-center items-center max-sm:h-[18rem] h-[23.5rem] text-white font-poppins">
+      <div className="bg-footer-blue flex flex-col justify-center items-center max-sm:h-[18rem] sm:h-[22rem] xl:h-[23.5rem] text-white font-poppins relative">
+        <img src="./tech-left.png" className="hidden lg:flex h-[22rem] xl:h-[23.5rem] absolute top-0 left-0 bottom-0"/>
+        <img src="./tech-right.png" className="hidden lg:flex h-[22rem] xl:h-[23.5rem] absolute top-0 right-0 bottom-0"/>
         <h1 className="text-base sm:text-3xl xl:text-5xl font-semibold mb-5">
         Shaping Tech Innovators Of Tommorrow
         </h1>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,10 +7,6 @@ export const About = () => {
 
       <Innovators />
 
-    <div className="flex flex-col justify-between md:px-12 md:py-6 px-5 xl:px-40 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins">
-
-    </div>
-
       <Values />
 
     </div>
