1711ed7d8a97fc634e0afb78d6b6cc18ae70889e
unknown <Adeleyeha.17@protonmail.com>
2023-11-14 16:27:27 +0100

    updated

--- src/components/Innovators/Innovators.tsx
+++ src/components/Innovators/Innovators.tsx
@@ -5,7 +5,7 @@ export const Innovators:React.FC = () => {
       <div className="bg-footer-blue flex flex-col justify-center items-center max-sm:h-[18rem] sm:h-[22rem] xl:h-[23.5rem] text-white font-poppins relative">
         <img src="./tech-left.png" className="hidden lg:flex h-[22rem] xl:h-[23.5rem] absolute top-0 left-0 bottom-0"/>
         <img src="./tech-right.png" className="hidden lg:flex h-[22rem] xl:h-[23.5rem] absolute top-0 right-0 bottom-0"/>
-        <h1 className="text-base sm:text-3xl xl:text-5xl font-semibold mb-5">
+        <h1 className="text-base sm:text-3xl xl:text-5xl font-medium mb-5">
         Shaping Tech Innovators Of Tommorrow
         </h1>
 
