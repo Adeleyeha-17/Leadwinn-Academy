3e7785647d5af01526b57c6f56bb8b1da37c69f3
unknown <Adeleyeha.17@protonmail.com>
2023-11-16 08:59:14 +0100

    updated IM

--- src/components/IgnitingMinds/IgnitingMind.tsx
+++ src/components/IgnitingMinds/IgnitingMind.tsx
@@ -4,8 +4,8 @@ export const IgnitingMind = () => {
     <div className="py-5 md:px-12 px-5 xl:px-40 lg:py-10 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins">
         <div className="flex max-sm:flex-col justify-center items-center gap-10 sm:px-16">
         <div className="text-head-black sm:w-[27.8125rem]">
-          <h1 className="text-xl lg:text-3xl xl:text-[2.375rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10">Igniting Minds and Fostering <span className="text-hero-blue">Innovators</span></h1>
-          <h4 className="text-xs lg:text-sm xl:text-base max-sm:pr-5">Leadwinn Academy passionately embraces a mission to kindle the sparks of curiosity and creativity in young minds through the realms of computer programming and software development. Our dedicated efforts are aimed at nurturing critical thinking and problem-solving skills, with the ultimate goal of transforming children, aged 6 to 17, into budding tech innovators and confident problem-solvers of the future</h4>
+          <h1 className="text-xl lg:text-3xl xl:text-[2.375rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10 max-lg:w-[14rem]">Igniting Minds and Fostering <span className="text-hero-blue">Innovators</span></h1>
+          <h4 className="text-xs lg:text-sm xl:text-base max-sm:pr-6">Leadwinn Academy passionately embraces a mission to kindle the sparks of curiosity and creativity in young minds through the realms of computer programming and software development. Our dedicated efforts are aimed at nurturing critical thinking and problem-solving skills, with the ultimate goal of transforming children, aged 6 to 17, into budding tech innovators and confident problem-solvers of the future</h4>
         </div>
 
         <div>
@@ -19,9 +19,9 @@ export const IgnitingMind = () => {
           <img src="./Empowering.png" className="w-[18.5rem] sm:w-[27.875rem] mx-auto"/>
         </div>
         
-        <div className="text-head-black w-[20rem] sm:w-[27.8125rem]">
-          <h1 className="text-xl lg:text-3xl xl:text-[2.375rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10">Empowering the  <span className="text-hero-blue">Future of Technology</span></h1>
-          <div className="text-xs lg:text-sm xl:text-base max-sm:pr-5">
+        <div className="text-head-black sm:w-[27.8125rem]">
+          <h1 className="text-xl lg:text-[2.1rem] xl:text-[2.375rem] lg:leading-[2.5rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10 max-lg:w-[14rem]">Empowering the  <span className="text-hero-blue">Future of Technology</span></h1>
+          <div className="text-xs lg:text-sm xl:text-base max-sm:pr-10">
             <h4 className="mb-5">Our Vision is to be the Premier Global Academy in Information Technology, Pioneering Excellence in Education, Innovation, and Leadership Worldwide.</h4>
 
 <h4>Our mission is to provide world-class education in information technology, foster innovation, cultivate ethical leadership, empower individuals to shape the future of technology, and promote global impact through excellence in education, research, and leadership development.
