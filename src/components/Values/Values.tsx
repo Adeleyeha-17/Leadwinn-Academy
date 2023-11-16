82004ee59ee9fc2c25f041b0cafea0c7aac13af5
unknown <Adeleyeha.17@protonmail.com>
2023-11-16 13:11:54 +0100

    updated

--- src/components/Values/Values.tsx
+++ src/components/Values/Values.tsx
@@ -1,7 +1,7 @@
 
 export const Values = () => {
   return (
-    <div className="bg-nav-blue h-[32rem] sm:h-[40rem] sm:max-lg:py-10 lg:h-[53rem] px-5 lg:space-x-20 md:px-12 flex flex-col justify-center items-center font-poppins text-head-black">
+    <div className="bg-nav-blue h-[32rem] sm:h-[49rem] lg:h-[53rem] px-5 lg:space-x-20 md:px-12 flex flex-col justify-center items-center font-poppins text-head-black">
       <h1 className="text-base sm:text-4xl font-semibold mb-2">Our Core Values</h1>
       <h4 className="text-xs sm:text-base mb-5 text-center">Our core mission is to empower every child for the tech-driven future.</h4>
 
