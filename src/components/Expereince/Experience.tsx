5f6e6a1c7f1df333b6e77ccef9faaae00003940f
unknown <Adeleyeha.17@protonmail.com>
2023-11-16 16:18:55 +0100

    updated

--- src/components/Expereince/Experience.tsx
+++ src/components/Expereince/Experience.tsx
@@ -2,8 +2,22 @@
 export const Experience = () => {
   return (
     <div className="bg-footer-blue w-full h-full mt-5 sm:mt-10">
-      <div className="py-10 md:px-12 px-5 xl:px-40 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl">
-         
+      <div className="py-10 md:px-12 px-5 xl:px-40 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl flex max-sm:flex-col justify-between">
+         <div>
+            <img src="./Experience.png" className="w-4/5 max-sm:mb-7"/>
+            </div>
+
+            <div className="text-white flex flex-col">
+               <h1 className="font-semibold text-xl lg:text-3xl xl:text-4xl w-52 lg:w-80 mb-5">Leadwinn Skills Experience</h1>
+               <ul className="flex flex-col gap-3 xl:gap-6 max-lg:text-xs text-lg">
+                  <li>Problem Solving</li>
+                  <li>Skills Assessment</li>
+                  <li>Gukded Learning Journeys</li>
+                  <li>Hands-On Practical Exercises</li>
+                  <li>Interactive Instructor-Led Sessions</li>
+               </ul>
+            </div>
+
       </div>
     </div>
   )
