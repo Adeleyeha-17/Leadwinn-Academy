d6e043ff6557fbc8537198a87a0776ca04828eff
unknown <Adeleyeha.17@protonmail.com>
2023-11-16 09:14:45 +0100

    updated IM

--- src/components/IgnitingMinds/IgnitingMind.tsx
+++ src/components/IgnitingMinds/IgnitingMind.tsx
@@ -5,7 +5,7 @@ export const IgnitingMind = () => {
         <div className="flex max-sm:flex-col justify-center items-center gap-10 sm:px-16">
         <div className="text-head-black sm:w-[27.8125rem]">
           <h1 className="text-xl lg:text-3xl xl:text-[2.375rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10 max-lg:w-[14rem]">Igniting Minds and Fostering <span className="text-hero-blue">Innovators</span></h1>
-          <h4 className="text-xs lg:text-sm xl:text-base max-sm:pr-6">Leadwinn Academy passionately embraces a mission to kindle the sparks of curiosity and creativity in young minds through the realms of computer programming and software development. Our dedicated efforts are aimed at nurturing critical thinking and problem-solving skills, with the ultimate goal of transforming children, aged 6 to 17, into budding tech innovators and confident problem-solvers of the future</h4>
+          <h4 className="text-xs lg:text-sm xl:text-base max-sm:pr-9">Leadwinn Academy passionately embraces a mission to kindle the sparks of curiosity and creativity in young minds through the realms of computer programming and software development. Our dedicated efforts are aimed at nurturing critical thinking and problem-solving skills, with the ultimate goal of transforming children, aged 6 to 17, into budding tech innovators and confident problem-solvers of the future</h4>
         </div>
 
         <div>
