35eedebe08b356773b99b46017dfde7280615fd9
unknown <Adeleyeha.17@protonmail.com>
2023-11-17 10:19:44 +0100

    updated

--- src/components/Expereince/Experience.tsx
+++ src/components/Expereince/Experience.tsx
@@ -9,10 +9,10 @@ export const Experience = () => {
 
             <div className="text-white flex flex-col">
                <h1 className="font-semibold text-xl lg:text-3xl xl:text-4xl sm:w-52 lg:w-80 mb-3 lg:mb-5">Leadwinn Skills Experience</h1>
-               <ul className="flex flex-col gap-2 lg:gap-3 max-lg:text-xs text-lg">
+               <ul className="flex flex-col gap-2 lg:gap-3 xl:gap-5 max-lg:text-xs text-lg">
                   <li>Problem Solving</li>
                   <li>Skills Assessment</li>
-                  <li>Gukded Learning Journeys</li>
+                  <li>Guided Learning Journeys</li>
                   <li>Hands-On Practical Exercises</li>
                   <li>Interactive Instructor-Led Sessions</li>
                </ul>
