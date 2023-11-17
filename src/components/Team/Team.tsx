2a0b0e2de9aea544f77eb1d3cc1429862089eee7
unknown <Adeleyeha.17@protonmail.com>
2023-11-17 15:34:49 +0100

    updated

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,38 +1,41 @@
+import React, { useState } from "react";
+
+interface TeamMember {
+  name: string;
+  img: string;
+  position: string;
+}
+
 export const Team = () => {
-
-  interface TeamMember {
-    name: string;
-    img: string;
-    position: string;
-  }
-
   const teamMember: TeamMember[] = [
     {
       name: "Adetokunbo Adeyemi",
       img: "./Tokunbo.png",
-      position: "Business Manager"
+      position: "Business Manager",
     },
     {
       name: "Adesubomi Fetuga",
       img: "./Subomi.png",
-      position: "International Facilitator"
+      position: "International Facilitator",
     },
     {
       name: "Adedamola Adeleye",
       img: "./Adeleye.png",
-      position: "International Facilitator"
+      position: "International Facilitator",
     },
     {
       name: "Abdulbasit Oyesiji",
       img: "./Basit.png",
-      position: "International Facilitator"
+      position: "International Facilitator",
     },
     {
       name: "Joshua Owonla",
       img: "./Joshua.png",
-      position: "Brand & Communication"
+      position: "Brand & Communication",
     },
   ];
+
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   return (
     <div className="font-poppins pb-10 md:px-12 px-5 py-10 xl:px-40 md:py-20 lg:max-xl:max-w-6xl mx-auto">
@@ -42,17 +45,24 @@ export const Team = () => {
       </div>
 
       <div className="flex max-sm:flex-col gap-5 lg:gap-10 sm:px-16 justify-center items-center">
-            <div>
-            <img src='./Tokunbo.png' className="w-[18.5rem] sm:w-[20rem] lg:w-[27.875rem]" />
-            </div>
-            
-            <div className="text-head-black lg:mt-6">
-              {teamMember.map((data, index) => (
-              <div key={index} className="flex items-center gap-10 xl:gap-20 border-b-2 mb-3 sm:mb-1 lg:mb-5">
-                <h1 className="w-20 sm:w-18 lg:w-28 text-center text-sm lg:text-base xl:text-lg font-semibold mb-3">{data.name}</h1>
-                <h4 className="mb-3 w-28 sm:w-24 lg:w-40 xl:w-48 text-xs lg:text-sm xl:text-base text-center">{data.position}</h4>
+        <div>
+          {selectedImage && (
+            <img src={selectedImage} className="w-[18.5rem] sm:w-[20rem] lg:w-[27.875rem]" alt="Selected Team Member" />
+          )}
+        </div>
 
-                <svg className="mb-3" width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="text-head-black lg:mt-6">
+          {teamMember.map((data, index) => (
+            <div key={index} className="flex items-center gap-10 xl:gap-20 border-b-2 mb-3 sm:mb-1 lg:mb-5">
+              <h1
+                className="w-20 sm:w-18 lg:w-28 text-center text-sm lg:text-base xl:text-lg font-semibold mb-3"
+                onClick={() => setSelectedImage(data.img)}
+              >
+                {data.name}
+              </h1>
+              <h4 className="mb-3 w-28 sm:w-24 lg:w-40 xl:w-48 text-xs lg:text-sm xl:text-base text-center">{data.position}</h4>
+
+              <svg className="mb-3" width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <rect x="0.5" y="1" width="39" height="39" rx="4.5" fill="white" />
                   <rect x="0.5" y="1" width="39" height="39" rx="4.5" stroke="#E5E7EB" />
                   <g clip-path="url(#clip0_725_4202)">
@@ -68,11 +78,10 @@ export const Team = () => {
                     </clipPath>
                   </defs>
                 </svg>
-
-              </div>
-              ))}
             </div>
-          </div>
+          ))}
+        </div>
       </div>
+    </div>
   );
 };
