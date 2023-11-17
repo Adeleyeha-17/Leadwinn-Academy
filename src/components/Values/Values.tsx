44effe6d9e5b2baf2cc2dee3ae6d228f6e09abd4
unknown <Adeleyeha.17@protonmail.com>
2023-11-17 11:01:15 +0100

    updated

--- src/components/Values/Values.tsx
+++ src/components/Values/Values.tsx
@@ -5,7 +5,7 @@ export const Values = () => {
       <h1 className="text-base sm:text-4xl font-semibold mb-2">Our Core Values</h1>
       <h4 className="text-xs sm:text-base mb-5 max-sm:text-center">Our core mission is to empower every child for the tech-driven future.</h4>
 
-      <img src="./values.png" alt="Core values" className="w-4/5 sm:w-[30rem]"/>
+      <img src="./values.png" alt="Core values" className="w-4/5 sm:w-[30rem] xl:w-[40rem]"/>
     </div>
   )
 }
