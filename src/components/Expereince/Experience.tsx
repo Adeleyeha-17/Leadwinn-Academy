df07ec9da6e0df4ae8ae9104724c84ddbda99ceb
unknown <Adeleyeha.17@protonmail.com>
2023-11-17 09:07:49 +0100

    Created The Team component

--- src/components/Expereince/Experience.tsx
+++ src/components/Expereince/Experience.tsx
@@ -4,12 +4,12 @@ export const Experience = () => {
     <div className="bg-footer-blue w-full h-full mt-5 sm:mt-10 font-poppins">
       <div className="py-10 md:px-12 px-5 xl:px-40 lg:py-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl flex max-sm:flex-col justify-between">
          <div>
-            <img src="./Experience.png" className="w-4/5 max-sm:mb-7"/>
+            <img src="./Experience.png" className="sm:w-3/5 lg:w-4/5 max-sm:mb-7"/>
             </div>
 
             <div className="text-white flex flex-col">
-               <h1 className="font-semibold text-xl lg:text-3xl xl:text-4xl w-52 lg:w-80 mb-5">Leadwinn Skills Experience</h1>
-               <ul className="flex flex-col gap-3 xl:gap-6 max-lg:text-xs text-lg">
+               <h1 className="font-semibold text-xl lg:text-3xl xl:text-4xl sm:w-52 lg:w-80 mb-3 lg:mb-5">Leadwinn Skills Experience</h1>
+               <ul className="flex flex-col gap-2 lg:gap-3 max-lg:text-xs text-lg">
                   <li>Problem Solving</li>
                   <li>Skills Assessment</li>
                   <li>Gukded Learning Journeys</li>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import Experience from "../components/Expereince"
 import IgnitingMind from "../components/IgnitingMinds"
 import Innovators from "../components/Innovators"
+import Team from "../components/Team"
 import Values from "../components/Values"
 
 export const About = () => {
@@ -14,6 +15,8 @@ export const About = () => {
       <Experience />
 
       <Values />
+
+      <Team />
 
     </div>
   )
