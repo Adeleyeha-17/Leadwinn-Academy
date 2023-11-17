269458deacfd9cfb499a765fe933dcc470cd9cab
unknown <Adeleyeha.17@protonmail.com>
2023-11-17 14:45:54 +0100

    updated

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -43,15 +43,14 @@ export const Team = () => {
 
       <div className="flex max-sm:flex-col gap-5 lg:gap-10 sm:px-16 justify-center items-center">
             <div>
-
-            <img src='./Tokunbo.png' className="w-[18.5rem] sm:w-[27.875rem]" />
+            <img src='./Tokunbo.png' className="w-[18.5rem] sm:w-[20rem] lg:w-[27.875rem]" />
             </div>
             
-            <div className="text-head-black mt-6">
+            <div className="text-head-black lg:mt-6">
               {teamMember.map((data, index) => (
-              <div key={index} className="flex items-center gap-10 xl:gap-20 border-b-2 mb-3 x:mb-5">
-                <h1 className="w-20 lg:w-28 text-center text-sm lg:text-base xl:text-lg font-semibold mb-3">{data.name}</h1>
-                <h4 className="mb-3 w-32 lg:w-40 xl:w-48 text-xs lg:text-sm xl:text-base text-center">{data.position}</h4>
+              <div key={index} className="flex items-center gap-10 xl:gap-20 border-b-2 mb-3 sm:mb-1 lg:mb-5">
+                <h1 className="w-20 sm:w-18 lg:w-28 text-center text-sm lg:text-base xl:text-lg font-semibold mb-3">{data.name}</h1>
+                <h4 className="mb-3 w-28 sm:w-24 lg:w-40 xl:w-48 text-xs lg:text-sm xl:text-base text-center">{data.position}</h4>
 
                 <svg className="mb-3" width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <rect x="0.5" y="1" width="39" height="39" rx="4.5" fill="white" />
