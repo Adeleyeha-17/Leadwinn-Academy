272989768a00c07831202d7a2eff8820ed4415c2
unknown <Adeleyeha.17@protonmail.com>
2023-11-17 10:42:41 +0100

    updated Experience

--- src/components/Expereince/Experience.tsx
+++ src/components/Expereince/Experience.tsx
@@ -4,11 +4,11 @@ export const Experience = () => {
     <div className="bg-footer-blue w-full h-full mt-5 sm:mt-10 font-poppins">
       <div className="md:px-12 px-5 py-10 xl:px-40 md:py-20 lg:max-xl:max-w-6xl mx-auto flex max-sm:flex-col justify-between">
          <div>
-            <img src="./Experience.png" className="sm:w-3/5 lg:w-4/5 max-sm:mb-7"/>
+            <img src="./Experience.png" className="w-3/5 sm:w-4/5 max-sm:mb-7"/>
             </div>
 
-            <div className="text-white flex flex-col justify-center items-center">
-               <h1 className="font-semibold text-xl lg:text-3xl xl:text-4xl sm:w-52 lg:w-80 mb-3 lg:mb-5">Leadwinn Skills Experience</h1>
+            <div className="text-white flex flex-col sm:justify-center sm:items-center">
+               <h1 className="font-semibold text-xl sm:text-2xl lg:text-3xl xl:text-4xl sm:w-52 lg:w-80 mb-3 lg:mb-5">Leadwinn Skills Experience</h1>
                <ul className="flex flex-col gap-2 max-lg:text-xs text-lg">
                   <li>Problem Solving</li>
                   <li>Skills Assessment</li>
