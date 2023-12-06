6460c317477e91f49f3b9bc4f1929cb5268b5d5e
unknown <Adeleyeha.17@protonmail.com>
2023-12-06 14:02:35 +0100

    updated

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -55,11 +55,11 @@ export const ForgotPassword: React.FC = () => {
         </div>
       </div>
 
-      <div className="relative w-full max-sm:h-screen lg:h-screen flex flex-col items-center pt-24 xl:pt-32">
+      <div className="relative w-full max-sm:h-screen lg:h-screen flex flex-col items-center pt-16 sm:pt-24 xl:pt-32">
 
       <div className="absolute top-16 right-8 sm:right-14 lg:right-14 mb-5">
        
-      <AiOutlineClose className="cursor-pointer" onClick={handleGoBack}/>
+      <AiOutlineClose className="cursor-pointer relative max-sm:-top-8" onClick={handleGoBack}/>
        
       </div>
 
