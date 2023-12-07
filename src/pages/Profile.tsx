5de8aa9ce51e4c15a566febae03d9c03a90d7940
unknown <Adeleyeha.17@protonmail.com>
2023-12-07 10:53:36 +0100

    updated

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -87,7 +87,7 @@ export const Profile: React.FC = () => {
 
 
   return (
-    < motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 2.5}} className="flex flex-col justify-between md:px-12 md:py-6 px-5 xl:px-40 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins">
+    < motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 2.5}} className="flex flex-col justify-between md:px-12 md:py-6 px-5 xl:px-40 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins mb-20">
       <div className="max-w-6xl mx-auto flex flex-col justify-center items-center">
       <section>
         <h1 className="text-3xl text-center font-bold mt-6 mb-3">Profile</h1>
@@ -99,17 +99,29 @@ export const Profile: React.FC = () => {
                   <img
                     src={URL.createObjectURL(selectedImage)}
                     alt="profile"
-                    className="rounded-full w-28 h-28 object-cover mb-5"
+                    className="rounded-full w-28 h-28 object-cover mb-4"
                   />
                 ) : (
                   <img
                     src={profileImage}
                     alt="profile"
-                    className="rounded-full w-28 h-28 object-cover mb-5"
+                    className="rounded-full w-28 h-28 object-cover mb-4"
                   />
                 )}
                {!selectedImage && (
-                  <input type="file" onChange={handleFileChange} />
+                <div className="mb-2">
+
+                  <label htmlFor="fileInput" className="fileInputLabel">
+                  Upload
+                </label>
+                <input
+                  type="file"
+                  id="fileInput"
+                  onChange={handleFileChange}
+                  className="fileInput"
+                  />
+                  </div>
+                
                 )}
               </div>
           {/* Name input */}
