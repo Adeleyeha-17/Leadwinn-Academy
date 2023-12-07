4490cee41951c3a93d62076655e11fd3123f113e
unknown <Adeleyeha.17@protonmail.com>
2023-12-07 15:30:31 +0100

    updated

--- src/pages/Techhub.tsx
+++ src/pages/Techhub.tsx
@@ -1,7 +1,11 @@
 import { motion } from "framer-motion"
+import comingSoon from "../assets/coming soon.png"
 
 export const Techhub = () => {
   return (
-    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 2.5}} className="flex flex-col justify-between md:px-12 md:py-6 px-5 xl:px-40 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins">Techhub</motion.div>
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 2.5}} className="flex flex-col justify-center items-center md:px-12 md:py-6 px-5 xl:px-40 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins h-screen text-5xl text-head-blue">
+
+<img src={comingSoon} className="max-lg:w-[30rem]" />    
+</motion.div>
   )
 }
