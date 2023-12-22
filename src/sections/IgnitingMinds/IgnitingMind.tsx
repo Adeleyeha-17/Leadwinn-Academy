a93bf8a0498e7cf5a7e41c8cfc4098376d3c33fb
unknown <Adeleyeha.17@protonmail.com>
2023-12-22 07:50:34 +0100

    update about

--- src/sections/IgnitingMinds/IgnitingMind.tsx
+++ src/sections/IgnitingMinds/IgnitingMind.tsx
@@ -4,44 +4,44 @@ export const IgnitingMind = () => {
   return (
     <div className="font-poppins pb-10 md:px-12 px-5 py-10 xl:px-40 md:py-20 lg:max-xl:max-w-6xl mx-auto">
       <div className="flex max-sm:flex-col justify-center items-center gap-10 sm:px-16">
-        <div className="text-head-black sm:w-[27.8125rem]">
-          <h1 className="text-xl lg:text-3xl xl:text-[2.375rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10 max-lg:w-[14rem]">Igniting Minds and Fostering <span className="text-hero-blue">Innovators</span></h1>
-          <h4 className="text-xs lg:text-sm xl:text-base max-sm:pr-9">Leadwinn Academy passionately embraces a mission to kindle the sparks of curiosity and creativity in young minds through the realms of computer programming and software development. Our dedicated efforts are aimed at nurturing critical thinking and problem-solving skills, with the ultimate goal of transforming children, aged 6 to 17, into budding tech innovators and confident problem-solvers of the future</h4>
+        <div className="text-head-black sm:w-[22rem] xl:w-[24rem]">
+          <h1 className="text-xl lg:text-3xl font-semibold mb-5 sm:mb-10 max-lg:w-[14rem]">Igniting Minds and Fostering <span className="text-hero-blue">Innovators</span></h1>
+          <h4 className="text-xs xl:text-sm max-sm:pr-9 ">Leadwinn Academy passionately embraces a mission to kindle the sparks of curiosity and creativity in young minds through the realms of computer programming and software development. Our dedicated efforts are aimed at nurturing critical thinking and problem-solving skills, with the ultimate goal of transforming children, aged 6 to 17, into budding tech innovators and confident problem-solvers of the future</h4>
         </div>
 
         <div>
-          <img src={enroll} className="w-[18.5rem] sm:w-[27.875rem] xl:w-[30rem]" />
+          <img src={enroll} className="w-[18.5rem] sm:w-[27rem]" />
         </div>
       </div>
 
 
       <div className="py-5 sm:py-20">
-        <div className="bg-nav-blue sm:max-lg:h-[11rem] lg:h-[13.125rem] w-1/2 sm:w-full rounded-[3.125rem] font-poppin mx-auto max-sm:mt-16 max-sm:py-10 flex justify-center items-center">
+        <div className="bg-nav-blue sm:max-lg:h-[11rem] lg:h-[10.125rem] w-1/2 sm:w-full rounded-[3.125rem] font-poppin mx-auto max-sm:mt-16 max-sm:py-10 flex justify-center items-center">
           <div className="items-center justify-evenly sm:flex sm:mt-3 w-full px-10 xl:px-20">
 
-            <div className="flex flex-col mb-10 sm:mb-0 items-center justify-cennter gap-5 sm:mt-5 lg:mt-6">
-              <h1 className="text-head-blue text-4xl xl:text-5xl font-medium">100+</h1>
-              <h4 className="w-[13rem] max-lg:text-xs text-base text-center">Trained students across 10 countries</h4>
+            <div className="flex flex-col mb-10 sm:mb-0 items-center justify-cennter gap-5 sm:mt-5">
+              <h1 className="text-head-blue text-4xl font-medium">100+</h1>
+              <h4 className="w-[13rem] text-xs text-center">Trained students across 10 countries</h4>
             </div>
             <div className="w-16 lg:w-40 mb-8 sm:mb-0 border-white border transform rotate-90 mx-auto rounded-lg"></div>
 
             <div className="flex flex-col mb-10 sm:mb-0 items-center justify-center gap-5">
-              <h1 className="text-head-blue text-4xl xl:text-5xl font-medium">50+</h1>
-              <h4 className="w-[10rem] max-lg:text-xs text-base text-center">projects submitted</h4>
+              <h1 className="text-head-blue text-4xl font-medium">50+</h1>
+              <h4 className="w-[10rem] text-xs text-center">projects submitted</h4>
             </div>
 
             <div className="w-16 lg:w-40 mb-8 sm:mb-0 border-white border transform rotate-90 mx-auto rounded-lg"></div>
 
             <div className="flex justify-center flex-col mb-8 sm:mb-0 items-center gap-5">
-              <h1 className="text-head-blue text-4xl xl:text-5xl font-medium">100+</h1>
-              <h4 className="w-[10rem] max-lg:text-xs text-base text-center">certificates earned</h4>
+              <h1 className="text-head-blue text-4xl font-medium">100+</h1>
+              <h4 className="w-[10rem] text-xs text-center">certificates earned</h4>
             </div>
 
             <div className="w-16 lg:w-40 mb-8 sm:mb-0 border-white border transform rotate-90 mx-auto rounded-lg"></div>
 
             <div className="flex justify-center flex-col mb-8 sm:mb-0 items-center gap-5">
-              <h1 className="text-head-blue text-4xl xl:text-5xl font-medium">99%</h1>
-              <h4 className="w-[13rem] max-lg:text-xs text-base text-center">of graduates recorded</h4>
+              <h1 className="text-head-blue text-4xl font-medium">99%</h1>
+              <h4 className="w-[13rem] text-xs text-center">of graduates recorded</h4>
             </div>
           </div>
 
@@ -51,12 +51,12 @@ export const IgnitingMind = () => {
 
       <div className="flex max-sm:flex-col-reverse sm:justify-center sm:items-center gap-10 sm:px-16 max-sm:mt-5">
         <div>
-          <img src={empowering} className="w-[18.5rem] sm:w-[27.875rem] xl:w-[30rem] mx-auto" />
+          <img src={empowering} className="w-[18.5rem] sm:w-[27rem] mx-auto" />
         </div>
 
-        <div className="text-head-black sm:w-[27.8125rem]">
-          <h1 className="text-xl lg:text-[2.1rem] xl:text-[2.375rem] lg:leading-[2.5rem] xl:leading-[3rem] font-bold mb-5 sm:mb-10 max-lg:w-[14rem] max-xl:w-[24rem]">Empowering the  <span className="text-hero-blue">Future of Technology</span></h1>
-          <div className="text-xs lg:text-sm xl:text-base max-sm:pr-10">
+        <div className="text-head-black sm:w-[22rem]">
+          <h1 className="text-xl lg:text-2xl font-bold mb-5 sm:mb-10 max-lg:w-[14rem] lg:w-[17rem]">Empowering the  <span className="text-hero-blue">Future of Technology</span></h1>
+          <div className="text-xs xl:text-sm max-sm:pr-10">
             <h4 className="mb-5">Our Vision is to be the Premier Global Academy in Information Technology, Pioneering Excellence in Education, Innovation, and Leadership Worldwide.</h4>
 
             <h4>Our mission is to provide world-class education in information technology, foster innovation, cultivate ethical leadership, empower individuals to shape the future of technology, and promote global impact through excellence in education, research, and leadership development.
