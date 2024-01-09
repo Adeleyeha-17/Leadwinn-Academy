ca396f3c698f1b2b6b66a1bf5cfdd0e8546353b5
unknown <Adeleyeha.17@protonmail.com>
2024-01-09 16:35:41 +0100

    update joinleads

--- src/sections/JoinLeads/JoinLeads.tsx
+++ src/sections/JoinLeads/JoinLeads.tsx
@@ -5,15 +5,17 @@ import { joinLeft, joinRight } from "../../assets/images"
 export const JoinLeads = () => {
   const joinStyle = {
     background: `url(${careerBg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   };
 
   return (
     <div>
-      <div className="w-full h-[32rem] bg-cover bg-center py-20 pl-52" style={joinStyle}>
-        <div className="bg-white text-home-gra opacity-80 w-[39rem] h-[22rem] rounded-2xl py-[3.75rem] px-10">
-          <h5 className="text-lg font-semibold w-72 mb-4">Curious About What Leadwinn Has To Offer?</h5>
+      <div className="w-full h-[14rem] sm:h-[24rem] lg:h-[27rem] xl:h-[32rem] py-16 xl:py-20 pl-16 lg:pl-24 xl:pl-52" style={joinStyle}>
+        <div className="bg-white text-home-gra opacity-80 w-[28rem] lg:w-[35rem] xl:w-[39rem] h-[15rem] lg:h-[19rem] xl:h-[22rem] rounded-2xl py-[1.9rem] lg:py-[3.75rem] px-10">
+          <h5 className="text-base lg:text-lg font-semibold w-64 lg:w-72 mb-4">Curious About What Leadwinn Has To Offer?</h5>
 
-          <p className="text-base font-normal w-[30rem] mb-10">
+          <p className="text-sm lg:text-base font-normal w-[24rem] lg:w-[30rem] mb-5 lg:mb-10">
           Explore our comprehensive brochure to uncover the learning map, detailed information, and the exciting opportunities that await you on your educational journey with Leadwinn.
           </p>
 
