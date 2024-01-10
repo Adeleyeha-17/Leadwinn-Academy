d3c118a911bc69c85604805b2b6d27f5d53b658a
unknown <Adeleyeha.17@protonmail.com>
2024-01-10 11:10:58 +0100

    uodated

--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -6,8 +6,8 @@ export const Career = () => {
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 2.5}} >
 <div className="bg-footer-blue flex flex-col justify-center items-center w-full h-full py-[6rem] xl:py-[8.75rem] text-white font-poppins relative overflow-hidden">
-        <img src={leftPlan} className="h-[8.5rem] sm:h-[13.5rem] lg:h-[18rem] xl:h-[23.1rem] absolute xl:top-8 left-0 bottom-0"/>
-        <img src={rightPlan} className="h-[8.5rem] sm:h-[13.5rem] lg:h-[18rem] xl:h-[23.1rem] absolute xl:top-8 right-0 bottom-0"/>
+        <img src={leftPlan} className="hidden lg:flex lg:h-[18rem] xl:h-[23.1rem] absolute xl:top-8 left-0 bottom-0"/>
+        <img src={rightPlan} className="hidden lg:flex lg:h-[18rem] xl:h-[23.1rem] absolute xl:top-8 right-0 bottom-0"/>
         <h1 className="text-base sm:text-2xl xl:text-4xl w-[19rem] sm:w-[45rem] mb-5 text-center font-semibold">
         Become a Crucial Part of our Educational Journey
         </h1>
