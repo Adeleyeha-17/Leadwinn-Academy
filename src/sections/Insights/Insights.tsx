deb4de03666f95402a4d7b6c467e95536e940585
unknown <Adeleyeha.17@protonmail.com>
2024-01-15 19:42:08 +0100

    update insights

--- src/sections/Insights/Insights.tsx
+++ src/sections/Insights/Insights.tsx
@@ -15,6 +15,34 @@ export const Insights = () => {
    return (
 
       <div className="h-full md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins mx-auto">
+
+<div className="py-5 sm:py-20">
+        <div className="bg-gradient-to-br from-[#0965CE] to-[#01152C] text-[#DCEDFF] sm:h-[9rem] w-1/2 sm:w-[45rem] rounded-[3.125rem] font-poppin mx-auto max-sm:mt-16 max-sm:py-10 flex justify-center items-center">
+          <div className="items-center justify-evenly w-full sm:flex px-1">
+
+            <div className="flex flex-col mb-10 sm:mb-0 items-center justify-center gap-5">
+              <h1 className="sm:max-lg:text-3xl text-4xl font-medium">1</h1>
+              <h4 className="w-[10rem] text-xs sm:max-lg:text-[0.65rem] text-center">Project</h4>
+            </div>
+
+            <div className="w-16 lg:w-40  mb-8 sm:mb-0 border-white border transform rotate-90 mx-auto rounded-lg"></div>
+
+            <div className="flex justify-center flex-col mb-8 sm:mb-0 items-center gap-5">
+              <h1 className="sm:max-lg:text-3xl text-4xl font-medium">5</h1>
+              <h4 className="w-[10rem] text-xs sm:max-lg:text-[0.65rem] text-center">Assignments</h4>
+            </div>
+
+            <div className="w-16 lg:w-40 mb-8 sm:mb-0 border-white border transform rotate-90 mx-auto rounded-lg"></div>
+
+            <div className="flex justify-center flex-col mb-8 sm:mb-0 items-center gap-5">
+              <h1 className="sm:max-lg:text-3xl text-4xl font-medium">1</h1>
+              <h4 className="w-[13rem] text-xs sm:max-lg:text-[0.65rem] text-center">Certificate</h4>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
          <div className="bg-gradient-to-br from-[#0965CE] to-[#01152C] h-full sm:h-[17.5rem] flex flex-col gap-10 sm:gap-8 justify-center items-center px-5 py-16 md:py-20 sm:px-20 lg:px-28">
             <h3 className="text-[#DCEDFF] text-base sm:text-3xl font-semibold">More Courses To Learn</h3>
 
