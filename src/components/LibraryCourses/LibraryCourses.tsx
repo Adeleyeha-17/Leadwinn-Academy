8c3857c763a9c7330ddc77445663683a01599232
unknown <Adeleyeha.17@protonmail.com>
2024-01-16 10:52:16 +0100

    update

--- src/components/LibraryCourses/LibraryCourses.tsx
+++ src/components/LibraryCourses/LibraryCourses.tsx
@@ -36,11 +36,11 @@ export const LibraryCourses = () => {
   
 
   return (
-    <div>
+    <div className='font-poppins'>
 
 
       <h4 className="bg-nav-blue text-heading text-center text-base sm:text-xl font-semibold pt-10 pb-5">Explore Different Learning Paths Within The Library</h4>
-    <div className="bg-nav-blue pb-10 flex gap-7 flex-col items-center justify-center font-poppins max-lg:overflow-x-auto">
+    <div className="bg-nav-blue pb-10 flex gap-7 flex-col items-center justify-center max-lg:overflow-x-auto">
 
       <div className="flex space-x-6">
         {libraryNav.map((nav, index) => (
@@ -53,8 +53,22 @@ export const LibraryCourses = () => {
 
       <div className={toggle === 0 ? "block" : "hidden"}>
 
-        SCRATCH
+          {/*Beginners Scratch*/}
+       <div className='h-[45.5rem] flex gap-3 flex-col items-center justify-center'>
+            <h4 className='text-heading text-base sm:text-2xl font-semibold'>Basic Scratch <span className='text-sm sm:text-xl text-[#999999] font-medium'>(6+ Years)</span></h4>
 
+            <p className='w-[22rem] sm:w-[46rem] xl:w-[57rem] text-center text-heading text-[0.8rem] xl:text-base'>Embark on a coding adventure with Leadwinn's MIT classes, specially crafted for young minds aged 6 to 8. Our program is designed to ignite curiosity and provide a solid foundation in computer science principles. Through engaging activities and hands-on learning, we inspire creativity and set the stage for a future filled with technological innovation.</p>
+       
+       <div className='w-[22rem] sm:w-[40rem] lg:w-[50rem] xl:w-[58rem] h-[23rem] mt-10 rounded-3xl border-4 border-blue-900'>
+
+       </div>
+       </div>
+
+          {/* Intermediate Scratch */}
+
+
+           {/* Advanced Scratch */}
+       
       </div>
 
       <div className={toggle === 1 ? "block" : "hidden"}>
