ec01efc90d0a58b992dbfbf1f1def81214356086
unknown <Adeleyeha.17@protonmail.com>
2024-01-16 11:03:52 +0100

    update insights component

--- src/sections/Insights/Insights.tsx
+++ src/sections/Insights/Insights.tsx
@@ -17,7 +17,7 @@ export const Insights = () => {
       <div className="h-full md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins mx-auto">
 
 <div className="py-10 sm:py-20">
-        <div className="bg-gradient-to-br from-[#0965CE] to-[#01152C] text-[#DCEDFF] sm:h-[9rem] w-1/2 sm:w-[38rem] lg:w-[45rem] rounded-[3.125rem] font-poppin mx-auto max-sm:mt-16 max-sm:py-10 flex justify-center items-center">
+        <div className="bg-gradient-to-br from-[#0965CE] to-[#01152C] text-[#DCEDFF] sm:h-[9rem] w-1/2 sm:w-[38rem] lg:w-[45rem] rounded-[3.125rem] font-poppin mx-auto max-sm:py-10 flex justify-center items-center">
           <div className="items-center justify-evenly w-full sm:flex px-1">
 
             <div className="flex flex-col mb-10 sm:mb-0 items-center justify-center sm:gap-5">
