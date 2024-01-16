c3e191d62f5211125d7629e78f6f80486788ec0c
unknown <Adeleyeha.17@protonmail.com>
2024-01-16 09:43:54 +0100

    update libcourses

--- src/components/LibraryCourses/LibraryCourses.tsx
+++ src/components/LibraryCourses/LibraryCourses.tsx
@@ -8,9 +8,6 @@ export const LibraryCourses = () => {
 
   const libraryNav: libraryNav[] = [
     {
-      link: '',
-    },
-    {
       link: 'Scratch',
     },
     {
@@ -30,11 +27,13 @@ export const LibraryCourses = () => {
     },
   ];
 
-  const [toggle, setToggle] = useState(1)
+  const [toggle, setToggle] = useState(0)
 
   const toggleTab = (index: number) => {
     setToggle(index)
   }
+
+  
 
   return (
     <div>
@@ -45,45 +44,45 @@ export const LibraryCourses = () => {
 
       <div className="flex space-x-6">
         {libraryNav.map((nav, index) => (
-          <h4 onClick={() => toggleTab(index)} key={index} className={`max-sm:mr-[28rem] w-max px-7 py-2 text-xs md:text-sm rounded-3xl font-semibold cursor-pointer ${toggle === index ? "bg-hero-blue text-white" : "bg-white text-heading"}`}> {nav.link} </h4>
+          <h4 onClick={() => toggleTab(index)} key={index} className={`ml-[34rem] sm:ml-[20rem] lg:ml-0 w-max px-7 py-2 text-xs md:text-sm rounded-3xl font-semibold cursor-pointer ${toggle === index ? "bg-hero-blue text-white" : "bg-white text-heading"}`}> {nav.link} </h4>
           ))}
           </div>
       </div>
 
       <div>
 
-      <div className={toggle === 1 ? "block" : "hidden"}>
+      <div className={toggle === 0 ? "block" : "hidden"}>
 
         SCRATCH
 
       </div>
 
-      <div className={toggle === 2 ? "block" : "hidden"}>
+      <div className={toggle === 1 ? "block" : "hidden"}>
 
         MIT
 
       </div>
 
 
-      <div className={toggle === 3 ? "block" : "hidden"}>
+      <div className={toggle === 2 ? "block" : "hidden"}>
 
         ROBLOX
 
       </div>
 
-      <div className={toggle === 4 ? "block" : "hidden"}>
+      <div className={toggle === 3 ? "block" : "hidden"}>
 
         UI/UX DESIGN
 
       </div>
 
-      <div className={toggle === 5 ? "block" : "hidden"}>
+      <div className={toggle === 4 ? "block" : "hidden"}>
 
         WEB DEVELOPMENT
 
       </div>
 
-      <div className={toggle === 6 ? "block" : "hidden"}>
+      <div className={toggle === 5 ? "block" : "hidden"}>
 
         PYTHON PROGRAMMING
 
