b17d4c5f1979c8f4b774d9c0d93b3fac45848a07
unknown <Adeleyeha.17@protonmail.com>
2024-01-26 08:27:41 +0100

    updates gif

--- src/assets/videos/index.tsx
+++ src/assets/videos/index.tsx
@@ -1,4 +1,4 @@
-import scratchBasic from "../videos/scratch basic.mp4";import scratchIntermediate from "../videos/scratch intermediate.mp4";import scratchAdvanced from "../videos/scratch advanced.mp4";import webBasic from "../videos/web basic.mp4";import webIntermediate from "../videos/web intermediate.mp4";import webAdvanced from "../videos/web advanced.mp4"
+import scratchBasic from "../videos/scratch basic.gif";import scratchIntermediate from "../videos/scratch intermediate.mp4";import scratchAdvanced from "../videos/scratch advanced.mp4";import webBasic from "../videos/web basic.mp4";import webIntermediate from "../videos/web intermediate.mp4";import webAdvanced from "../videos/web advanced.mp4"
 export {scratchBasic,scratchIntermediate,scratchAdvanced,webBasic,webIntermediate,webAdvanced,}
 
 
