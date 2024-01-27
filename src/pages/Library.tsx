24752b4455fbf6075d0ee628c1942f506bfcbcc7
unknown <Adeleyeha.17@protonmail.com>
2024-01-27 16:50:07 +0100

    update

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion"
 import { Insights, Knowledge } from "../sections"
 import React from "react"
 import LibraryCourses from "../components/LibraryCourses";
@@ -6,7 +5,7 @@ import DownloadBrochure from "../components/DownloadBrochure";
 
 export const Library: React.FC = () => {
   return (
-    < motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0 }}>
+    < div>
       <Knowledge />
 
       <LibraryCourses />
@@ -14,6 +13,6 @@ export const Library: React.FC = () => {
       <Insights />
 
       <DownloadBrochure />
-    </motion.div>
+    </div>
   )
 }
