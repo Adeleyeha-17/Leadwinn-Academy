98b245c313ad8f5f09479b1ed7f7ee7c988dde76
unknown <Adeleyeha.17@protonmail.com>
2024-01-27 16:47:49 +0100

    update

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -6,7 +6,7 @@ import DownloadBrochure from "../components/DownloadBrochure";
 
 export const Library: React.FC = () => {
   return (
-    < motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1.2 }}>
+    < motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0 }}>
       <Knowledge />
 
       <LibraryCourses />
