9ba8f95dc84f19afcd6cd5ab77756b8dc9bc23ed
unknown <Adeleyeha.17@protonmail.com>
2024-01-28 10:54:57 +0100

    update

--- src/components/LibraryCourses/LibraryCourses.tsx
+++ src/components/LibraryCourses/LibraryCourses.tsx
@@ -12,7 +12,7 @@ export const LibraryCourses = () => {
     setToggle(index)
   }
 
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
 
   useEffect(() => {
     const handleResize = () => {
