14395a42af76e0e41b7e1e3b71a1592fe7866110
unknown <Adeleyeha.17@protonmail.com>
2024-01-28 10:16:35 +0100

    update

--- src/assets/videos/index.tsx
+++ src/assets/videos/index.tsx
@@ -1,4 +1,4 @@
-import scratchBasic from "../videos/scratch basic.mp4";import scratchIntermediate from "../videos/scratch intermediate.mp4";import scratchAdvanced from "../videos/scratch advanced.mp4";import webBasic from "../videos/web basic.mp4";import webIntermediate from "../videos/web intermediate.mp4";import webAdvanced from "../videos/web advanced.gif";import pythongif from "../videos/python.gif";import drawStar from "../videos/draw stars.gif";
+import scratchBasic from "../videos/scratch basic.mp4";import scratchIntermediate from "../videos/scratch intermediate.mp4";import scratchAdvanced from "../videos/scratch advanced.mp4";import webBasic from "../videos/web basic.gif";import webIntermediate from "../videos/web intermediate.mp4";import webAdvanced from "../videos/web advanced.gif";import pythongif from "../videos/python.gif";import drawStar from "../videos/draw stars.gif";
 export {scratchBasic,scratchIntermediate,scratchAdvanced,webBasic,webIntermediate,webAdvanced,pythongif,drawStar}
 
 
