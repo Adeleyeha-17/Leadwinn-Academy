5ad15b387c0bfe734140a2f4c283168151b19a14
unknown <Adeleyeha.17@protonmail.com>
2024-02-06 19:44:52 +0100

    update

--- src/sections/Courses/Courses.tsx
+++ src/sections/Courses/Courses.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+
 export const Courses = () => { 
 
     interface Course {
@@ -20,7 +21,7 @@ export const Courses = () => {
   }, [])
 
   const vanElements = vans.map(van => (
-      <div key={van.id} className="w-[22rem] sm:w-[25rem] h-[30rem] rounded-3xl border px-6 py-10">
+      <div key={van.id} className="w-[22rem] xl:w-[25rem] h-[30rem] rounded-3xl border px-6 py-10">
           <img alt={van.name} src={van.imageUrl} className="rounded-xl mb-4 transform transition duration-150 ease-in-out hover:"/>
           <div className="van-info">
           <i className={`border py-1 px-3 text-white bg-${van.color} rounded-md`}>{van.type}</i>
@@ -31,11 +32,12 @@ export const Courses = () => {
   ))
 
   return (
-      <div className="flex flex-col items-center px-10">
+      <div className="flex flex-col items-center justify-center">
           <h1>FEATURED COURSES</h1>
           <h4>Find Yours From The Featured</h4>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 items-center justify-center gap-5">
               {vanElements}
+      
           </div>
       </div>
   )
