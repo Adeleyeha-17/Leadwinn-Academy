0d3a420ae8d23f8359b07ba0e3af8371db4b092d
unknown <Adeleyeha.17@protonmail.com>
2024-02-14 08:03:21 +0100

    update

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,7 +1,7 @@
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom"
 import { edu1, edu2, edu3, home } from "../../assets/images"
 import { MdAirplay } from "react-icons/md";
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
 
 export const Hero: React.FC = () => {
 
@@ -10,15 +10,28 @@ export const Hero: React.FC = () => {
       <div className="flex flex-col lg:flex-row items-center lg:space-x-5 md:px-12 pt-28 sm:pb-10 lg:pb-20 xl:pb-0 px-5 xl:px-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl mx-auto font-poppins">
         <div className="font-poppins text-heading lg:w-1/2">
           <h1
-            className="max-lg:text-center max-sm:text-2xl sm:max-lg:text-3xl lg:max-xl:text-[2.3125rem] xl:max-2xl:text-[2.53125rem] font-bold mb-6 sm:mx-0">Limitless learning at your fingertips</h1>
-          <h1 className="mb-6 max-sm:text-center text-base lg:text-sm xl:text-lg lg:max-w-[30rem] xl:max-w-[36rem]">Online learning and teaching marketplace with courses & over 100 students. Taught by experts to help you acquire new skills.</h1>
+            className="max-lg:text-center max-sm:text-2xl  sm:max-xl:text-[2.3125rem] xl:max-2xl:text-[2.53125rem] font-bold mb-6 sm:mx-0">Limitless learning at your fingertips</h1>
+          <h1 className="mb-6 max-lg:text-center sm:max-lg:mx-auto text-base lg:text-sm xl:text-lg sm:max-w-[38rem] lg:max-w-[30rem] xl:max-w-[36rem]">Online learning and teaching marketplace with courses & over 100 students. Taught by experts to help you acquire new skills.</h1>
 
-          <div className="flex text-xs sm:text-base gap-3 mb-7">
-            <h5>Learn with experts</h5>
+          <div className="flex flex-wrap items-center max-lg:justify-center text-xs xl:text-base gap-3 mb-7">
+            
+            <div className="flex items-center gap-2">
+                <IoCheckmarkDoneCircleOutline className="w-6 h-6"/> 
+              <h5>
+                Learn with experts</h5>
+              </div>
+
+              <div className="flex items-center gap-2">
+              <IoCheckmarkDoneCircleOutline className="w-6 h-6"/>
             <h5>Get certificate</h5>
-            <h5>Get membership</h5>
+              </div>
+
+              <div className="flex items-center gap-2">
+              <IoCheckmarkDoneCircleOutline className="w-6 h-6"/>
+            <h5> Get membership</h5>
+              </div>
           </div>
-          <div className="flex max-sm:flex-col gap-5 items-center sm:gap-10 mb-10">
+          <div className="flex max-sm:flex-col gap-5 items-center max-lg:justify-center sm:gap-10 mb-10">
 
             <Link to="/courses" className={`flex justify-center items-center max-sm:w-40 py-4 px-8 bg-head-blue text-white  text-xs font-semibold rounded-xl transition hover:bg-blue-600 ease-in-out duration-300 cursor-pointer`}>Get Started</Link>
 
@@ -27,10 +40,10 @@ export const Hero: React.FC = () => {
 
         </div>
         <div className="lg:w-1/2">
-          <motion.img
+          <img
             src={home}
             alt="home illustration"
-            className="mx-auto "
+            className="mx-auto home__img"
             loading="eager" />
         </div>
       </div>
