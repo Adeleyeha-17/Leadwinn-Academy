e9945cc81d7203c7b0b3fcc86e6cd17d21feada5
unknown <Adeleyeha.17@protonmail.com>
2024-02-29 11:09:52 +0100

    update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,6 +6,7 @@ import { User } from "@supabase/supabase-js";
 
 export const Profile: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [profilePictureUrl, setProfilePictureUrl] = useState<string | null>(null);
   const [formData, setFormData] = useState({
     fullName: "",
     email: ""
@@ -26,7 +27,9 @@ export const Profile: React.FC = () => {
             fullName: data.user.user_metadata.full_name || "",
             email: data.user.email || ""
           });
-         
+          if (data.user.user_metadata.picture) {
+            setProfilePictureUrl(data.user.user_metadata.picture);
+          }
         } else {
           navigate("/sign-in");
         }
@@ -34,10 +37,9 @@ export const Profile: React.FC = () => {
         console.error("Error fetching user data:", (error as Error).message);
       }
     };
-  
+    
     fetchUserData();
   }, [navigate, user?.id]);
-
 
   return (
     <motion.div
@@ -48,27 +50,17 @@ export const Profile: React.FC = () => {
       className="flex flex-col justify-between md:px-12 px-5 xl:px-40 pt-20 md:max-lg:max-w-6xl lg:max-xl:max-w-7xl font-poppins mb-20"
     >
       <div className="max-w-6xl mx-auto flex flex-col justify-center items-center">
+        {profilePictureUrl && (
+          <img src={profilePictureUrl} alt="Profile" className="rounded-full w-40 h-40 mb-4" />
+        )}
         <section>
           <h1 className="text-3xl text-center font-bold mt-6 mb-3">Profile</h1>
-
-          <div className="sm:w-full md:w-[85%] mt-6 px-3 mx-auto">
-          <h4>Welcome to your Leadwinn profile,</h4>
-            <form>
-              <input
-                type="text"
-                name="fullName"
-                value={fullName}
-                className={`w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out `}
-              />
-
-              <input
-                type="email"
-                name="email"
-                value={email}
-                className={`w-full rounded text-xl text-gray-700 bg-white px-4 py-2 my-2 border border-gray-300 transition ease-in-out`}
-              />
-              
-            </form>
+          <div className="mt-6 px-3 mx-auto">
+            <h4 className="text-lg">Welcome to your Leadwinn profile,</h4>
+            <div>
+              <h4>Name: <span>{fullName}</span></h4>
+              <h4>Email Address: <span>{email}</span></h4>
+            </div>
           </div>
         </section>
       </div>
