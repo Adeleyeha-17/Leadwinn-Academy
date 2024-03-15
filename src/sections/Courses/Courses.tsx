901002cc54dd49f2f0645a44093c365f9ddba4e7
unknown <Adeleyeha.17@protonmail.com>
2024-03-15 14:14:15 +0100

    update

--- src/sections/Courses/Courses.tsx
+++ src/sections/Courses/Courses.tsx
@@ -24,16 +24,37 @@ export const Courses = () => {
 
     const settings = {
       dots: true,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 3500,
-      pauseOnHover: true,
-      pauseOnFocus: true,
-      cssEase: "linear"
+      slidesToScroll: 2,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
 
     const [vans, setVans] = useState<Course[]>([])
@@ -43,8 +64,8 @@ export const Courses = () => {
           .then(data => setVans(data.vans))
   }, [])
 
-  const vanElements = vans.map(van => (
-      <div key={van.id} className="w-[22rem] xl:w-[25rem] h-[30rem] xl:h-[32rem] rounded-3xl border px-6 py-10">
+  const vanElements = vans.slice(0, 9).map(van => (
+      <div key={van.id} className="w-[22rem] xl:w-[25rem] h-[30rem] xl:h-[32rem] rounded-3xl border px-6 py-10 ml-10">
             <Link to={`/courses/${van.id}`}>
           <img alt={van.name} src={van.imageUrl} className="rounded-xl mb-4 transform transition duration-150 ease-in-out hover:"/>
           <div>
@@ -79,7 +100,7 @@ export const Courses = () => {
           <h5 className="px-2 sm:max-w-[36rem] text-center text-heading mb-10">From coding to design and beyond, find the perfect course to elevate your expertise and achieve your goals.</h5>
         </div>
         <div className="">
-          <Slider {...settings} className="">
+          <Slider {...settings}>
         {vanElements}
         </Slider>
     </div>
